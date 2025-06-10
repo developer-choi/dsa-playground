35ee9eaef1d23954d302ce713905ca10e756aea8
Yu Jin Choe <forworkchoe@gmail.com>
2025-06-10 21:08:34 +0900

    버블소트를 긱스포긱스에 맞게 수정

--- src/examples/algorithm/sort/bubble.ts
+++ src/examples/algorithm/sort/bubble.ts
@@ -2,6 +2,7 @@ import {SortParam} from './index';
 
 export {};
 
+// https://www.geeksforgeeks.org/bubble-sort-algorithm
 function main() {
   console.log(bubbleSort({
     order: 'asc',
@@ -20,9 +21,12 @@ main();
  *************************************************************************************************************/
 function bubbleSort({order, value}: SortParam): number[] {
   const result: number[] = [...value];
+  let swapped = false;
 
   // Point 1. 배열에 요소가 n개면, 순회는 n-1번만 해도 됨. 제일 마지막 거는 순회 안해도 어차피 제일 왼쪽에 있을거니까.
   for (let i = 0; i < value.length - 1; i++) {
+    swapped = false;
+
     // Point 2. 여기서 - 1을 더 해줘야, undefined가 안나옴.
     for (let j = 0; j < value.length - i - 1; j++) {
       const left = result[j];
@@ -31,10 +35,16 @@ function bubbleSort({order, value}: SortParam): number[] {
 
       if (isTrue) {
         [result[j], result[j + 1]] = [result[j + 1], result[j]];
+        swapped = true;
       }
     }
 
     console.log(`${i} loop`, result);
+
+    // Best case에서 Time Complexity를 O(n)으로 최적화 하는 방법
+    if (!swapped) {
+      break;
+    }
   }
 
   return result;
