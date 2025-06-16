10524016a849b94ea3a5414118f99e3921dacc79
Yu Jin Choe <forworkchoe@gmail.com>
2025-06-16 22:28:47 +0900

    Quick Sort에서 Lomuto Partition주석 보강

--- src/examples/algorithm/sort/quick.ts
+++ src/examples/algorithm/sort/quick.ts
@@ -18,7 +18,7 @@ export default function quickSort({value: original, order}: SortParam): SortResu
       return;
     }
 
-    const pivotIndex = partition({
+    const pivotIndex = lomutoPartition({
       order,
       value,
       endIndex,
@@ -60,21 +60,30 @@ export default function quickSort({value: original, order}: SortParam): SortResu
   };
 }
 
-/** partition() 시나리오
+/** lomutoPartition() 시나리오
  * 1. 피벗 결정하고 (첫, 마지막, 중간, 랜덤 등)
  * 2. 원본 배열을 직접 수정해서 [피벗보다 작은값들], 피벗, [피벗보다 큰값들] 순서로 만든 다음
  * 3. 피벗의 최종 index를 반환함.
  */
-function partition({startIndex, endIndex, value: original, order}: InternalQuickSortParam): number {
+function lomutoPartition({startIndex, endIndex, value: original, order}: InternalQuickSortParam): number {
   const pivotIndex = endIndex; // 긱스포긱스 예제 처럼 제일 마지막 요소를 피벗으로 선택하기로 함.
   const pivotValue = original[pivotIndex];
 
+  /**
+   * Pivot 보다 작은 요소만 모아놓기 위한 포인터.
+   * 이 포인터와 같거나 좌측에 있으면 전부 피벗보다 작음.
+   */
   let i = startIndex - 1;
-  let j = startIndex;
 
-  while (j !== pivotIndex) {
+  let j = startIndex; // Pivot 보다 작은 요소가 있는지 찾기 위해 계속 우측으로 이동한느 포인터
+
+  while (j < pivotIndex) {
     const isTrue = order === 'asc' ? original[j] < pivotValue : original[j] > pivotValue;
 
+    /**
+     * pivot보다 작은 요소 (original[j]) 를 찾았기 때문에,
+     * 그 요소를 아직 정렬되지않은 요소 (original[i+1])와 바꾸고 i 포인터를 우측으로 한칸 더 옮기기 위함.
+     */
     if (isTrue) {
       i++;
       [original[i], original[j]] = [original[j], original[i]];
