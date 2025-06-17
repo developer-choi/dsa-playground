4f2315b668d68b7951bea1087d5920daac58f5c2
Yu Jin Choe <forworkchoe@gmail.com>
2025-06-17 20:59:50 +0900

    H-index 풀이법에 주석 추가

--- src/coding-test/sort/h-index.ts
+++ src/coding-test/sort/h-index.ts
@@ -1,7 +1,16 @@
 export {};
 
-// https://school.programmers.co.kr/learn/courses/30/lessons/42747
+/**
+ * URL = https://school.programmers.co.kr/learn/courses/30/lessons/42747
+ *
+ * 접근방법)
+ * h의 최대값은 배열의 길이값.
+ * h의 최소값은 0. [0, 0, 0, 0] ==> 0
+ * 그러므로 h값은 0부터 배열의 길이값만큼만 따져보면 됨.
+ */
 function solution(numbers: number[]): number {
+  // 근데 h의 최대값을 찾으라고 했기 때문에, 역순으로 순회를 하되, 0은 순회할 필요 없이 그냥 return 0 하면 됨.
+  // 잘 생각해보면, 0은 언제나 H-index가 될 수 있기 때문임.
   for (let i = numbers.length; i > 0; i--) {
     const isTrue = condition(numbers, i);
 
@@ -13,7 +22,17 @@ function solution(numbers: number[]): number {
   return 0;
 }
 
-// array가 boundary 이상인 값이 boundary개 이상이고, 나머지 (boundary 미만) 값의 갯수가 boundary값 보다 이하라면 true
+/**
+ * h번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용되었다면
+ * ⇒
+ * 나머지 = h보다 작은 배열 요소들 이라는 뜻이므로, 위 문장에 단어를 대체하면
+ *
+ * “[h보다 작은 배열 요소들]이 h번 이하 인용되었다면” 가 됨.
+ * ⇒
+ * h보다 작은 배열 요소의 갯수가 h값 이하라면 라는 말이 됨.
+ *
+ * 즉, 조건 = array가 boundary 이상인 값이 boundary개 이상이고, 나머지 (boundary 미만) 값의 갯수가 boundary값 보다 이하라면 true
+ */
 function condition(array: number[], boundary: number): boolean {
   let sameOrBiggerCount = 0;
 
