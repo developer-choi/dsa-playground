702b9890424a764fb5eebbcd2fa0f24b47464b4d
Yu Jin Choe <forworkchoe@gmail.com>
2025-06-24 20:31:55 +0900

    GPT 피드백 2차 반영 > 최악의 케이스 ([2, 2, 2], 2) 에서 O(n)이 되는 문제 해결

--- src/examples/algorithm/search/find-first-and-last.ts
+++ src/examples/algorithm/search/find-first-and-last.ts
@@ -1,46 +1,60 @@
 // https://www.geeksforgeeks.org/dsa/find-first-and-last-positions-of-an-element-in-a-sorted-array/
 export default function findFirstAndLast(array: number[], target: number): {firstIndex: number, lastIndex: number} {
-  const index = binarySearch(array, 0, array.length - 1, target);
-
-  if (index === -1) {
-    return {
-      lastIndex: -1,
-      firstIndex: -1
-    };
-  }
-
-  let firstIndex = index;
-  let lastIndex = index;
-
-  while (firstIndex >= 0 && array[firstIndex] === target) {
-    firstIndex--;
-  }
-
-  while (lastIndex <= array.length - 1 && array[lastIndex] === target) {
-    lastIndex++;
-  }
+  const firstIndex = binarySearch(array, 0, array.length - 1, target, 'first');
+  const lastIndex = binarySearch(array, 0, array.length - 1, target, 'last');
 
   return {
-    firstIndex: firstIndex + 1,
-    lastIndex: lastIndex - 1
+    lastIndex,
+    firstIndex
   };
 }
 
-function binarySearch(array: number[], startIndex: number, endIndex: number, target: number) {
-  if (startIndex >= endIndex && array[endIndex] !== target) {
-    return -1;
+function binarySearch(array: number[], startIndex: number, endIndex: number, target: number, mode: 'first' | 'last') {
+  if (startIndex >= endIndex) {
+    if (array[endIndex] !== target) {
+      return -1;
+    } else {
+      return endIndex;
+    }
   }
 
   const middleIndex = Math.floor((startIndex + endIndex) / 2);
   const middleValue = array[middleIndex];
 
-  if (middleValue === target) {
-    return middleIndex;
-  }
+  // 중복된 수가 없는 배열에서의 이진탐색 처럼 탐색
+  if (middleValue !== target) {
+    if (middleValue > target) {
+      return binarySearch(array, startIndex, middleIndex - 1, target, mode);
+    } else {
+      return binarySearch(array, middleIndex + 1, endIndex, target, mode);
+    }
 
-  if (middleValue > target) {
-    return binarySearch(array, startIndex, middleIndex - 1, target);
+    // 중복된 수가 있는 배열에서는 가장 왼쪽 혹은 가장 오른쪽으로 한번 더 찾아야함
   } else {
-    return binarySearch(array, middleIndex + 1, endIndex, target);
+    let index: number;
+
+    if (mode === 'first') {
+      index = binarySearch(array, startIndex, middleIndex - 1, target, mode);
+    } else {
+      index = binarySearch(array, middleIndex + 1, endIndex, target, mode);
+    }
+
+    // 더 작거나 (mode === 'first') 더 큰 (mode === 'last') 숫자를 찾으려고 헀는데 못찾았으면, 직전 middleIndex값이 boundary라는 뜻
+    if (index === -1) {
+      return middleIndex;
+    } else {
+      return index;
+    }
   }
 }
+
+/* if (index === -1) 이 조건이 참이 되는 테스트 케이스 데이터
+array = [
+  0, 0, 1, 1, 2, 2, 3, 3, 4, 4,
+  5, 5, 6, 6, 7, 7, 8, 8, 9, 9,
+  10, 10, 11, 11, 12, 12, 13, 13, 14, 14,
+  15, 15, 16, 16, 17, 17, 18, 18, 19, 19,
+  20, 20, 21, 22
+]
+target = 1
+ */
