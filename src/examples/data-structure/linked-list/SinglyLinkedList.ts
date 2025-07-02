615335932a24e3e493df232810a0a849659ad91c
Yu Jin Choe <forworkchoe@gmail.com>
2025-07-02 21:03:00 +0900

    insertAt() 메소드 구현

--- src/examples/data-structure/linked-list/SinglyLinkedList.ts
+++ src/examples/data-structure/linked-list/SinglyLinkedList.ts
@@ -51,6 +51,52 @@ export default class SinglyLinkedList {
   }
 
   /**
+   * [Head]
+   * URL: https://www.geeksforgeeks.org/insert-a-node-at-front-beginning-of-a-linked-list/
+   * Time Complexity: O(1), We have a pointer to the head and we can directly attach a node and update the head pointer
+   *
+   * [between]
+   * URL: https://www.geeksforgeeks.org/dsa/insert-a-node-at-a-specific-position-in-a-linked-list/
+   * Time Complexity: O(n), where n is the number of nodes in the linked list.
+   *
+   * [Tail]
+   * URL: https://www.geeksforgeeks.org/insert-node-at-the-end-of-a-linked-list/
+   * Time Complexity: O(N) where N is the length of the linked list
+   */
+  insertAt(index: number, data: number) {
+    let currentIndex = 0;
+
+    this.traverse({
+      onLoop: (meta) => {
+        if (currentIndex === index) {
+          const newNode = new Node(data);
+
+          switch (meta.type) {
+            case 'head': {
+              this.head = newNode;
+              this.head.next = meta.node;
+              break;
+            }
+            case 'between': {
+              meta.beforeNode.next = newNode;
+              newNode.next = meta.node;
+              break;
+            }
+
+            case 'tail':
+              meta.node.next = newNode;
+              break;
+          }
+          return true;
+        } else {
+          currentIndex++;
+          return false;
+        }
+      }
+    });
+  }
+
+  /**
    * https://www.geeksforgeeks.org/dsa/search-an-element-in-a-linked-list-iterative-and-recursive/
    * Time Complexity: O(n), where n is the number of nodes in the linked list.
    */
@@ -58,7 +104,7 @@ export default class SinglyLinkedList {
     let index = -1;
 
     this.traverse({
-      onLoop: node => {
+      onLoop: ({node}) => {
         index++;
         return node.data === data;
       },
@@ -78,13 +124,26 @@ export default class SinglyLinkedList {
     let length = 0;
 
     this.traverse({
-      onLoop: (node) => {
+      onLoop: ({node}) => {
         length++;
         return !node.next;
       },
     });
 
     return length;
+  }
+
+  toString(): string {
+    const array: number[] = [];
+
+    this.traverse({
+      onLoop: ({node}) => {
+        array.push(node.data);
+        return false;
+      }
+    });
+
+    return array.join(',');
   }
 
   private getTail(): Node | undefined {
@@ -101,15 +160,18 @@ export default class SinglyLinkedList {
 
   private traverse({onLoop, onLoopEnd}: TraverseParameter) {
     let pointer: Node | undefined = this.head;
+    let beforeNode: Node | undefined = undefined;
 
     while (pointer) {
-      const needToReturn = onLoop?.(pointer);
+      const type: NodeType = pointer === this.head ? 'head' : pointer.next ? 'between' : 'tail';
+      const needToReturn = onLoop?.({node: pointer, beforeNode: beforeNode as any, type});
 
       if (needToReturn) {
         return;
       }
 
       if (pointer.next) {
+        beforeNode = pointer;
         pointer = pointer.next;
       } else {
         break;
@@ -120,16 +182,27 @@ export default class SinglyLinkedList {
   }
 }
 
+type TraversingMeta =  {
+  node: Node;
+  beforeNode: Node | undefined;
+  type: Extract<NodeType, 'head'>;
+} | {
+  node: Node;
+  beforeNode: Node;
+  type: Exclude<NodeType, 'head'>;
+}
+
 interface TraverseParameter {
-  onLoop?: (node: Node) => boolean; // head부터 tail까지 1번씩 실행됨.
+  onLoop?: (meta: TraversingMeta) => boolean; // head부터 tail까지 1번씩 실행됨.
   onLoopEnd?: (tail: Node | undefined) => void; // head조차 없으면 undefined일 수 있음.
 }
 
+type NodeType = 'head' | 'between' | 'tail';
+
 const list = new SinglyLinkedList();
 list.push(1);
-list.push(2);
 list.push(3);
-console.log(list.get(0));
-console.log(list.get(1));
-console.log(list.get(2));
-console.log(list.get(3));
+list.push(4);
+list.insertAt(0, 0);
+list.insertAt(2, 2);
+list.insertAt(4, 5);
