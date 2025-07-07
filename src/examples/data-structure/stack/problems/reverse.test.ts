4b3c884a6664f5dcc50ba9d97e1405a7275c6729
Yu Jin Choe <forworkchoe@gmail.com>
2025-07-07 20:55:02 +0900

    Stack 뒤집기 구현

---
+++ src/examples/data-structure/stack/problems/reverse.test.ts
@@ -0,0 +1,16 @@
+import StackUsingLinkedList from '@/examples/data-structure/stack/StackUsingLinkedList';
+import {reverseStackUsingRecursive} from '@/examples/data-structure/stack/problems/reverse';
+
+// yarn test src/examples/data-structure/stack/problems/reverse.test.ts
+describe('reverseStackUsingRecursive()', () => {
+  it('stack을 뒤집을 수 있어야 한다.', () => {
+    const stack = new StackUsingLinkedList<number>();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    const reversedStack = reverseStackUsingRecursive(stack);
+    expect(reversedStack.pop()).toBe(1);
+    expect(reversedStack.pop()).toBe(2);
+    expect(reversedStack.pop()).toBe(3);
+  });
+});
