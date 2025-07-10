47728e4267fc54b3258131713fe27bb50088f658
Yu Jin Choe <forworkchoe@gmail.com>
2025-07-10 22:04:00 +0900

    Stock Span에서 GPT 피드백 반영 > 어차피 topValue는 topIndex로 알아낼 수 있으니까 굳이 target까지 저장할 필요는 없다.

--- src/examples/data-structure/stack/problems/nearest/stock-span.ts
+++ src/examples/data-structure/stack/problems/nearest/stock-span.ts
@@ -26,29 +26,22 @@ export function stockSpanUsingLoop(input: number[]): number[] {
   return answer;
 }
 
-interface Candidate {
-  value: number;
-  index: number;
-}
-
 export function stockSpanUsingStack(input: number[]): number[] {
-  // Point 3. candidate는 value와 index 둘 다 저장한다. 연속적인 값의 갯수를 세지않고, 좌측으로 가장 가깝게 더 큰 값을 찾아서 index 서로 빼면 그 사이 갯수 나오니까.
-  const candidates = new StackUsingLinkedList<Candidate>();
+  // Point 3. candidate는 index만 저장한다. 연속적인 값의 갯수를 세지않고, 좌측으로 가장 가깝게 더 큰 값을 찾아서 index 서로 빼면 그 사이 갯수 나오니까.
+  const candidates = new StackUsingLinkedList<number>();
   const result: number[] = [];
 
   for (let i = 0; i < input.length; i++) {
     const target = input[i];
 
     while (candidates.length) {
-      const top = candidates.peek() as Candidate;
+      const topIndex = candidates.peek() as number;
+      const topValue = input[topIndex];
 
-      if (top.value > target) {
-        result.push(i - top.index);
+      if (topValue > target) {
+        result.push(i - topIndex);
 
-        candidates.push({
-          value: target,
-          index: i
-        });
+        candidates.push(i);
         break;
       } else {
         /** Point 2. pop() 한 값이, 조건에 안맞으면 걍 버려도 됨.
@@ -64,10 +57,7 @@ export function stockSpanUsingStack(input: number[]): number[] {
        * 그래서 비교를 훨씬 덜 해도 됨.
        * 그대신 후보를 아주 까다로운 조건으로 따져서 넣는거임.
        */
-      candidates.push({
-        value: target,
-        index: i
-      });
+      candidates.push(i);
       // Point 4. target 보다 큰 수가 왼쪽에 하나도없다는 뜻이기 때문에, target의 index가 곧 연속 증가 갯수를 뜻함.
       result.push(i + 1);
     }
