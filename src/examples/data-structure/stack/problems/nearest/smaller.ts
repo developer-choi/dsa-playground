adb33aa5e0d61d9aa1fdbae097c8cf337e128c97
Yu Jin Choe <forworkchoe@gmail.com>
2025-07-10 21:24:40 +0900

    좌측에서 가장 가까운 작은값 찾기 문제를 내 스타일로 다시 변경

--- src/examples/data-structure/stack/problems/nearest/smaller.ts
+++ src/examples/data-structure/stack/problems/nearest/smaller.ts
@@ -11,26 +11,27 @@ export default function findNearestSmallerNumberOnLeftSide(input: number[]): num
   for (let i = 0; i < input.length; i++) {
     const target = input[i];
 
-    /** Point 2. pop() 한 값이, 조건에 안맞으면 걍 버려도 됨. (else 문 따로 없음)
-     * 1. target 보다 우측에 있는 놈들의 가장 가깝게 작은놈이 "최소 target임"
-     * 2. pop() 했는데 target 보다 크다면, 후보조차 될 수 없음. target이 되면 됐지.
-     */
-    while (candidates.length && (candidates.peek() as number) >= target) {
-      candidates.pop();
-    }
-
-    if (candidates.length > 0) {
+    // Point 1. 후보에서 나보다 작은 값 찾기. 배열의 좌측을 다 탐색하는게 아니라, 후보만 뒤질거임.
+    while (candidates.length) {
       const top = candidates.peek() as number;
-      result.push(top);
-    } else {
-      result.push(-1);
+
+      if (top < target) {
+        result.push(top);
+        candidates.push(target);
+        break;
+      } else {
+        /** Point 2. 조건에 안맞으면 걍 버려도 됨.
+         * 1. target 보다 우측에 있는 놈들의 가장 가깝게 작은놈이 "최소 target임"
+         * 2. 지금 pop() 했는데 target 보다 크다면, 후보조차 될 수 없음. target이 되면 됐지.
+         */
+        candidates.pop();
+      }
     }
 
-    /** Point 1. 배열의 좌측을 다 탐색하는게 아니라, 후보만 추려서 뒤질거임.
-     * 그래서 비교를 훨씬 덜 해도 됨.
-     * 그대신 후보를 아주 까다로운 조건으로 따져서 넣는거임.
-     */
-    candidates.push(target);
+    if (candidates.length === 0) {
+      result.push(-1);
+      candidates.push(target);
+    }
   }
 
   return result;
