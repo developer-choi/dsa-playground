bc6988c8d940f12a54fc9a268ba4131883503fb8
Yu Jin Choe <forworkchoe@gmail.com>
2025-07-11 12:56:26 +0900

    배포 같이되는 갯수 구하기 문제 2회차 풀이. 배열 순회를 1번만 허면서도, 저장해야하는 데이터도 remain / togetherDeployCount 2개가 아니라 togetherDeployCount 1개만 저장할 수 있도록 개선

--- src/coding-test/stack/level2/42586.ts
+++ src/coding-test/stack/level2/42586.ts
@@ -1,57 +1,32 @@
 export {};
 
-interface Candidate {
-  remain: number;
-  togetherDeployCount: number;
-}
-
 /** 의도
  * 처음부터 순회 돌면서
  * 남은 날짜 구하고
- * candidates에 저장하고
+ * 최근 남은 날짜 변수에 저장하고
  * 그다음 순회해서
  * 또 남은잘짜 구하고
- * 가장 최근에 저장한거 꺼내와서
+ * 최근 남은날짜랑 비교해서
  * 작으면 남은날짜 +1 하고
  * 크면 1 새로 넣기
- *
- * 다른 아이디어)
- * 순회를 총 2번할 까 생각도 했었음.
- * 1회차 > 남은일 수만 따로 계산해두고
- * 2회차 > 그 남은일수로 연산하기
- *
- * 하지만 난 순회 1번만 하고싶었음.
- * 물론 마지막엔 그냥 편하게 리턴하려고 candidates 배열 map 돌면서 마무리하긴했지만,
- * 이것도 candidates 저장할 때 answer도 같이 저장하면 그만이긴했음.
  */
 // URL: https://school.programmers.co.kr/learn/courses/30/lessons/42586
 function solution(progresses: number[], speeds: number[]): number[] {
-  const candidates: Candidate[] = [];
+  const answer: number[] = [];
+  let latestBiggestRemain: number | undefined = undefined;
 
   for (let i = 0; i < progresses.length; i++) {
     const remain = Math.ceil((100 - progresses[i]) / speeds[i]);
 
-    if (candidates.length === 0) {
-      candidates.push({
-        remain,
-        togetherDeployCount: 1
-      });
-      continue;
-    }
-
-    const top = candidates[candidates.length - 1];
-
-    if (top.remain >= remain) {
-      candidates[candidates.length - 1].togetherDeployCount++;
+    if (latestBiggestRemain !== undefined && latestBiggestRemain >= remain) {
+      answer[answer.length - 1]++;
     } else {
-      candidates.push({
-        remain,
-        togetherDeployCount: 1
-      });
+      answer.push(1);
+      latestBiggestRemain = remain;
     }
   }
 
-  return candidates.map(candidate => candidate.togetherDeployCount);
+  return answer;
 }
 
 /*************************************************************************************************************
