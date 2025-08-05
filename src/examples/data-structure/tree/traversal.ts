89eda7e5ee7ae36330682809ddefefa2ada139fd
Yu Jin Choe <forworkchoe@gmail.com>
2025-08-06 00:19:35 +0900

    트리 순회관련 문제를 주석 필기로 대체

--- src/examples/data-structure/tree/traversal.ts
+++ src/examples/data-structure/tree/traversal.ts
@@ -10,9 +10,21 @@ import {BinaryTreeNode} from '@/examples/data-structure/tree/index';
  * Time Complexity: O(n) ==> n개 노드 순회하는데 O(n)이 걸림.
  * Auxiliary Space: O(h) ==> 재귀라서 레벨 h 만큼의 스택이 생기기 때문. 최악은 O(n). skewed tree
  */
-export function depthFirstTraversal<D>(node: BinaryTreeNode<D>, traversal: 'inorder' | 'preorder' | 'postorder') {
+export function depthFirstTraversal<D>(root: BinaryTreeNode<D> | undefined, traversal: 'inorder' | 'preorder' | 'postorder') {
+  if (!root) {
+    return {
+      array: [],
+      maxHeight: -1
+    };
+  }
+
   const array: D[] = [];
+
+  // traversal 로직 하나로 이만큼의 추가정보를 알아낼 수 있다는 뜻
   let maxHeight = 0;
+  // let length, 순회하면서 하나씩 더하면 전체 노드의 갯수 알아낼 수 있고, > https://www.geeksforgeeks.org/dsa/write-a-c-program-to-calculate-size-of-a-tree/
+  // n보다 크거나 작거나 같은 탐색도 순회하면서 할 수 있고 > https://www.geeksforgeeks.org/dsa/find-maximum-or-minimum-in-binary-tree/
+  // let sum = 0; 노드 총합 다 더하는것도 당연히 가능하고 > https://www.geeksforgeeks.org/dsa/check-if-a-given-binary-tree-is-sumtree/
 
   function inorder(current: BinaryTreeNode<D>, height: number) {
     maxHeight = Math.max(height, maxHeight);
@@ -60,7 +72,7 @@ export function depthFirstTraversal<D>(node: BinaryTreeNode<D>, traversal: 'inor
     postorder
   };
 
-  map[traversal](node, 0);
+  map[traversal](root, 0);
 
   return {
     array,
@@ -73,7 +85,7 @@ export function depthFirstTraversal<D>(node: BinaryTreeNode<D>, traversal: 'inor
  * Doc: https://docs.google.com/document/d/1MzkBVNfFktmMl-0uR1oO31fDxC7LM47cI0Q8Kv6PWxU/edit?tab=t.0
  *
  * Time Complexity: O(n) ==> 모든 노드 1번씩 순회하는데 전부 1번씩만 순회했음.
- * Auxiliary Space: O(n/2) ==> O(n), 가장 메모리가 클 때는 Complete Binary Tree에서 가장 마지막 레벨 순회할 때, 이 때 노드갯수는 전체갯수의 약 1/2 임.
+ * Auxiliary Space: O(n/2) ==> O(n), 가장 메모리를 많이 쓸 때는 Complete Binary Tree에서 가장 마지막 레벨 순회할 때, 이 때 노드갯수는 전체갯수의 약 1/2 임.
  */
 export function* breadthFirstTraversal<D>(root: BinaryTreeNode<D> | undefined): Generator<{
   node: BinaryTreeNode<D>,
