97b129a65722ce6c8b956c81c413c72c649fa265
Yu Jin Choe <forworkchoe@gmail.com>
2025-08-05 21:30:33 +0900

    LinkedListBinaryTree 에서 내부적으로 순회 시 parent도 같이 전달하도록 수정

--- src/examples/data-structure/tree/linked-list.ts
+++ src/examples/data-structure/tree/linked-list.ts
@@ -34,13 +34,13 @@ export class LinkedListBinaryTree<D> extends BinaryTree<D> {
    * Time Complexity: O(n) ==> 모든 노드 1번씩 순회하는데 전부 1번씩만 순회했음.
    * Auxiliary Space: O(n/2) ==> O(n), 가장 메모리가 클 때는 Complete Binary Tree에서 가장 마지막 레벨 순회할 때, 이 때 노드갯수는 전체갯수의 약 1/2 임.
    */
-  private* traverse(): Generator<{node: BinaryTreeNode<D>, level: number, index: number}, void, undefined> {
+  private* traverse(): Generator<{node: BinaryTreeNode<D>, parent: InternalInterationItem<D>['parent'], level: number, index: number}, void, undefined> {
     if (!this.root) {
       return;
     }
 
     // 탐색해야하는 노드들
-    const nextSearchQueue: BinaryTreeNode<D>[] = [this.root];
+    const nextSearchQueue: InternalInterationItem<D>[] = [{node: this.root, parent: undefined}];
     let level = 0;
     let index = 0;
 
@@ -48,18 +48,18 @@ export class LinkedListBinaryTree<D> extends BinaryTree<D> {
       const iterativeCount = nextSearchQueue.length;
 
       for (let i = 0; i < iterativeCount; i++) {
-        const node = nextSearchQueue.shift() as BinaryTreeNode<D>;
+        const {node, parent} = nextSearchQueue.shift() as InternalInterationItem<D>;
 
-        yield {node, level, index};
+        yield {node, parent, level, index};
         index++;
 
         // 다음 라벨에 또 순회해야하니 다음 레벨 노드들 저장
         if (node.left) {
-          nextSearchQueue.push(node.left);
+          nextSearchQueue.push({node: node.left, parent: {node, direction: 'left'}});
         }
 
         if (node.right) {
-          nextSearchQueue.push(node.right);
+          nextSearchQueue.push({node: node.right, parent: {node, direction: 'right'}});
         }
       }
       level++;
@@ -71,4 +71,12 @@ export class LinkedListBinaryTree<D> extends BinaryTree<D> {
       yield {data: node.data, level, index};
     }
   }
+}
+
+interface InternalInterationItem<D> {
+  node: BinaryTreeNode<D>,
+  parent: undefined | {
+    node: BinaryTreeNode<D>;
+    direction: 'left' | 'right';
+  };
 }
