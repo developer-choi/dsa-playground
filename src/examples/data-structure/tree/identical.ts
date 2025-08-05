1eed124ad3a1ee9aa783aa63f7d9a9018c284e3b
Yu Jin Choe <forworkchoe@gmail.com>
2025-08-06 00:31:58 +0900

    Tree 동일한지 체크하는 문제 GPT 피드백 반영

--- src/examples/data-structure/tree/identical.ts
+++ src/examples/data-structure/tree/identical.ts
@@ -4,43 +4,30 @@ import {BinaryTreeNode} from '@/examples/data-structure/tree/index';
  * URL: https://www.geeksforgeeks.org/dsa/write-c-code-to-determine-if-two-trees-are-identical/
  * Doc: https://docs.google.com/document/d/1hmQ93jf-hPjph7pKNf1hPJkwa-THOQS3iI7lYYnExTM/edit?tab=t.0
  */
-export function areTreesIdenticalWithDFS<D>(root1: BinaryTreeNode<D>, root2: BinaryTreeNode<D>): boolean {
-  function recursive(node1: BinaryTreeNode<D>, node2: BinaryTreeNode<D>): boolean {
+export function areTreesIdenticalWithDFS<D>(root1: BinaryTreeNode<D> | undefined, root2: BinaryTreeNode<D> | undefined): boolean {
+  function recursive(node1: BinaryTreeNode<D> | undefined, node2: BinaryTreeNode<D> | undefined): boolean {
+    if (node1 === undefined && node2 === undefined) {
+      return true;
+    }
+
     if (!areNodesIdentical(node1, node2)) {
       return false;
     }
 
-    let identical: boolean | undefined;
-
-    if (node1.left) {
-      identical = recursive(node1.left, node2.left as BinaryTreeNode<D>);
-    }
-
-    if (identical === false) {
-      return false;
-    }
-
-    if (node1.right) {
-      identical = recursive(node1.right, node2.right as BinaryTreeNode<D>);
-    }
-
-    if (identical === false) {
-      return false;
-    }
-
-    return true;
+    // Point 여기서 === 연산자 쓰면 안되고, && 써야함. 둘 다 true여야하기 때문.
+    return recursive(node1?.left, node2?.left) && recursive(node1?.left, node2?.left)
   }
 
   return recursive(root1, root2);
 }
 
-export function areTreesIdenticalWithBFS<D>(root1: BinaryTreeNode<D>, root2: BinaryTreeNode<D>): boolean {
-  if (root1.data !== root2.data) {
+export function areTreesIdenticalWithBFS<D>(root1: BinaryTreeNode<D> | undefined, root2: BinaryTreeNode<D> | undefined): boolean {
+  if (!areNodesIdentical(root1, root2)) {
     return false;
   }
 
-  let nextSearchQueue1: BinaryTreeNode<D>[] = [root1];
-  let nextSearchQueue2: BinaryTreeNode<D>[] = [root2];
+  let nextSearchQueue1: BinaryTreeNode<D>[] = [root1 as BinaryTreeNode<D>];
+  let nextSearchQueue2: BinaryTreeNode<D>[] = [root2 as BinaryTreeNode<D>];
 
   while (nextSearchQueue1.length) {
     let iterativeCount = nextSearchQueue1.length;
@@ -72,6 +59,14 @@ export function areTreesIdenticalWithBFS<D>(root1: BinaryTreeNode<D>, root2: Bin
  * 데이터는 같아야하고,
  * left / right는 없을거면 다같이없고, 있을거면 다같이 있어야함.
  */
-function areNodesIdentical<D>(node1: BinaryTreeNode<D>, node2: BinaryTreeNode<D>) {
-  return node2.data === node1.data && !!node2.left == !!node1.left && !!node2.right === !!node1.right;
+function areNodesIdentical<D>(node1: BinaryTreeNode<D> | undefined, node2: BinaryTreeNode<D> | undefined) {
+  if (node1 === undefined && node2 === undefined) {
+    return true;
+  }
+
+  if (node1 !== undefined && node2 !== undefined) {
+    return node2.data === node1.data && !!node2.left == !!node1.left && !!node2.right === !!node1.right;
+  }
+
+  return false;
 }
