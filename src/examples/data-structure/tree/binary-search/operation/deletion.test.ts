dd1509169939dc79047b9ae479bf6d0b016224d9
Yu Jin Choe <forworkchoe@gmail.com>
2025-08-13 20:56:18 +0900

    기존 내 코드에 큰 오류를 발견하여 테스트 코드를 새로 작성 (대체할 노드의 자식이 있는 경우 제대로 옮기지 못함)

--- src/examples/data-structure/tree/binary-search/operation/deletion.test.ts
+++ src/examples/data-structure/tree/binary-search/operation/deletion.test.ts
@@ -8,224 +8,129 @@ const algorithms = [
 ];
 
 // yarn test src/examples/data-structure/tree/binary-search/operation/deletion.test.ts
-describe.each(algorithms)('Insertion BST > $name', ({fn}) => {
-  let root: BinaryTreeNode<number> | undefined;
-
-  beforeEach(() => {
-    root = new BinaryTreeNode(50);
-
-    // level 1
-    root.left = new BinaryTreeNode(30);
-    root.right = new BinaryTreeNode(70);
-
-    // level2
-    root.left.left = new BinaryTreeNode(20);
-    root.left.right = new BinaryTreeNode(40);
-    root.right.left = new BinaryTreeNode(60);
-    root.right.right = new BinaryTreeNode(80);
-
-    // level 3
-    root.right.left.left = new BinaryTreeNode(59);
-    root.right.left.right = new BinaryTreeNode(61);
-    root.right.right.left = new BinaryTreeNode(75);
-    root.right.right.right = new BinaryTreeNode(85);
-
-    // level4
-    root.right.right.left.left = new BinaryTreeNode(73);
-    root.right.right.right.left = new BinaryTreeNode(83);
-    root.right.right.right.right = new BinaryTreeNode(86);
-
-    // level5
-    root.right.right.left.left.right = new BinaryTreeNode(74);
+describe.each(algorithms)('공식문서에서 제시한 Deletion BST Cases > $name', ({fn}) => {
+  it('Case 1. 삭제할 노드가 리프노드여도 삭제가 잘 되야함', () => {
+    const root = new BinaryTreeNode(50);
+    root.left = new BinaryTreeNode(40);
+    root.right = new BinaryTreeNode(60);
+    fn(root, 60);
+    expect(summarizeBinaryTree(root)).toEqual([
+      {level: 0, data: 50, direction: undefined, parent: undefined},
+      {level: 1, data: 40, direction: 'left', parent: 50},
+    ]);
   });
 
-  it('루트 노드를 삭제해도 잘 삭제되야한다.', () => {
-    fn(root, 50);
+  it('Case 2-1. 삭제할 노드의 자식이 1개 이면서, 대체할 노드에도 자식이 있는 경우 대체할 노드의 자식의 연결방향이 잘 유지되야한다.', () => {
+    const root = new BinaryTreeNode(50);
+    root.right = new BinaryTreeNode(60);
+    root.right.right = new BinaryTreeNode(70);
+    fn(root, 60);
     expect(summarizeBinaryTree(root)).toEqual([
-      {level: 0, data: 59, direction: undefined, parent: undefined},
-      {level: 1, data: 30, direction: 'left', parent: 59},
-      {level: 1, data: 70, direction: 'right', parent: 59},
+      {level: 0, data: 50, direction: undefined, parent: undefined},
+      {level: 1, data: 70, direction: 'right', parent: 50},
+    ]);
+  });
+
+  it('Case 2-2. 삭제할 노드의 자식이 1개 이면서, 대체할 노드에도 자식이 있는 경우 대체할 노드의 자식의 연결방향이 잘 유지되야한다.', () => {
+    const root = new BinaryTreeNode(50);
+    root.right = new BinaryTreeNode(60);
+    root.right.right = new BinaryTreeNode(70);
+    root.right.right.left = new BinaryTreeNode(65);
+    root.right.right.right = new BinaryTreeNode(75);
+    fn(root, 60);
+    expect(summarizeBinaryTree(root)).toEqual([
+      {level: 0, data: 50, direction: undefined, parent: undefined},
+      {level: 1, data: 70, direction: 'right', parent: 50},
+      {level: 2, data: 65, direction: 'left', parent: 70},
+      {level: 2, data: 75, direction: 'right', parent: 70},
+    ]);
+  });
+
+  it('Case 3-1. 삭제할 노드의 자식이 2개 이면서, 대체할 노드에 자식이 없는 경우 삭제가 잘 되야한다.', () => {
+    const root = createDeepBST();
+
+    fn(root, 80);
+    expect(summarizeBinaryTree(root)).toEqual([
+      {level: 0, data: 50, direction: undefined, parent: undefined},
+      {level: 1, data: 30, direction: 'left', parent: 50},
+      {level: 1, data: 70, direction: 'right', parent: 50},
       {level: 2, data: 20, direction: 'left', parent: 30},
       {level: 2, data: 40, direction: 'right', parent: 30},
       {level: 2, data: 60, direction: 'left', parent: 70},
-      {level: 2, data: 80, direction: 'right', parent: 70},
+      {level: 2, data: 83, direction: 'right', parent: 70},
+      {level: 3, data: 59, direction: 'left', parent: 60},
       {level: 3, data: 61, direction: 'right', parent: 60},
-      {level: 3, data: 75, direction: 'left', parent: 80},
-      {level: 3, data: 85, direction: 'right', parent: 80},
+      {level: 3, data: 75, direction: 'left', parent: 83},
+      {level: 3, data: 85, direction: 'right', parent: 83},
       {level: 4, data: 73, direction: 'left', parent: 75},
-      {level: 4, data: 83, direction: 'left', parent: 85},
       {level: 4, data: 86, direction: 'right', parent: 85},
       {level: 5, data: 74, direction: 'right', parent: 73},
     ]);
   });
 
-  describe('root 노드 기준 오른쪽 노드 삭제하기', () => {
-    it('삭제를 성공하면 루트 노드를 반환해야한다.', () => {
-      expect(fn(root, 86)?.data).toBe(50);
-    });
+  it('Case 3-2. 삭제할 노드의 자식이 2개 이면서, 대체할 노드에 자식이 있는 경우 삭제가 잘 되야한다.', () => {
+    const root = createDeepBST();
 
-    it('삭제할 노드가 리프노드여도 삭제가 잘 되야함', () => {
-      fn(root, 86);
-      expect(summarizeBinaryTree(root)).toEqual([
-        {level: 0, data: 50, direction: undefined, parent: undefined},
-        {level: 1, data: 30, direction: 'left', parent: 50},
-        {level: 1, data: 70, direction: 'right', parent: 50},
-        {level: 2, data: 20, direction: 'left', parent: 30},
-        {level: 2, data: 40, direction: 'right', parent: 30},
-        {level: 2, data: 60, direction: 'left', parent: 70},
-        {level: 2, data: 80, direction: 'right', parent: 70},
-        {level: 3, data: 59, direction: 'left', parent: 60},
-        {level: 3, data: 61, direction: 'right', parent: 60},
-        {level: 3, data: 75, direction: 'left', parent: 80},
-        {level: 3, data: 85, direction: 'right', parent: 80},
-        {level: 4, data: 73, direction: 'left', parent: 75},
-        {level: 4, data: 83, direction: 'left', parent: 85},
-        {level: 5, data: 74, direction: 'right', parent: 73},
-      ]);
-    });
-
-    it('삭제할 노드가 자식은 있는데 손자가 없는 경우에도 잘 삭제되야함.', () => {
-      fn(root, 85);
-      expect(summarizeBinaryTree(root)).toEqual([
-        {level: 0, data: 50, direction: undefined, parent: undefined},
-        {level: 1, data: 30, direction: 'left', parent: 50},
-        {level: 1, data: 70, direction: 'right', parent: 50},
-        {level: 2, data: 20, direction: 'left', parent: 30},
-        {level: 2, data: 40, direction: 'right', parent: 30},
-        {level: 2, data: 60, direction: 'left', parent: 70},
-        {level: 2, data: 80, direction: 'right', parent: 70},
-        {level: 3, data: 59, direction: 'left', parent: 60},
-        {level: 3, data: 61, direction: 'right', parent: 60},
-        {level: 3, data: 75, direction: 'left', parent: 80},
-        {level: 3, data: 86, direction: 'right', parent: 80},
-        {level: 4, data: 73, direction: 'left', parent: 75},
-        {level: 4, data: 83, direction: 'left', parent: 86},
-        {level: 5, data: 74, direction: 'right', parent: 73},
-      ]);
-    });
-
-    it('삭제할 노드가 손자이상이 있는 경우에도 잘 삭제되야함.', () => {
-      fn(root, 80);
-      expect(summarizeBinaryTree(root)).toEqual([
-        {level: 0, data: 50, direction: undefined, parent: undefined},
-        {level: 1, data: 30, direction: 'left', parent: 50},
-        {level: 1, data: 70, direction: 'right', parent: 50},
-        {level: 2, data: 20, direction: 'left', parent: 30},
-        {level: 2, data: 40, direction: 'right', parent: 30},
-        {level: 2, data: 60, direction: 'left', parent: 70},
-        {level: 2, data: 83, direction: 'right', parent: 70},
-        {level: 3, data: 59, direction: 'left', parent: 60},
-        {level: 3, data: 61, direction: 'right', parent: 60},
-        {level: 3, data: 75, direction: 'left', parent: 83},
-        {level: 3, data: 85, direction: 'right', parent: 83},
-        {level: 4, data: 73, direction: 'left', parent: 75},
-        {level: 4, data: 86, direction: 'right', parent: 85},
-        {level: 5, data: 74, direction: 'right', parent: 73},
-      ]);
-    });
-
-    it('삭제할 노드가 손자도 있고, 대체할 노드에도 자식이 있는 경우에도 잘 삭제되야함.', () => {
-      fn(root, 70);
-      expect(summarizeBinaryTree(root)).toEqual([
-        {level: 0, data: 50, direction: undefined, parent: undefined},
-        {level: 1, data: 30, direction: 'left', parent: 50},
-        {level: 1, data: 73, direction: 'right', parent: 50},
-        {level: 2, data: 20, direction: 'left', parent: 30},
-        {level: 2, data: 40, direction: 'right', parent: 30},
-        {level: 2, data: 60, direction: 'left', parent: 73},
-        {level: 2, data: 80, direction: 'right', parent: 73},
-        {level: 3, data: 59, direction: 'left', parent: 60},
-        {level: 3, data: 61, direction: 'right', parent: 60},
-        {level: 3, data: 75, direction: 'left', parent: 80},
-        {level: 3, data: 85, direction: 'right', parent: 80},
-        {level: 4, data: 74, direction: 'left', parent: 75},
-        {level: 4, data: 83, direction: 'left', parent: 85},
-        {level: 4, data: 86, direction: 'right', parent: 85},
-      ]);
-    });
-
-    it('삭제 할 노드가 오른쪽에 자식이 없고 왼쪽에는 있는 경우에도 잘 삭제되야한다.', () => {
-      fn(root, 86); // 86이 없는 상태를 셋팅해놓고
-      fn(root, 85); // 그러고나서 85를 삭제한 경우 삭제할 85 노드가 83으로 대체되는지 체크
-      expect(summarizeBinaryTree(root)).toEqual([
-        {level: 0, data: 50, direction: undefined, parent: undefined},
-        {level: 1, data: 30, direction: 'left', parent: 50},
-        {level: 1, data: 70, direction: 'right', parent: 50},
-        {level: 2, data: 20, direction: 'left', parent: 30},
-        {level: 2, data: 40, direction: 'right', parent: 30},
-        {level: 2, data: 60, direction: 'left', parent: 70},
-        {level: 2, data: 80, direction: 'right', parent: 70},
-        {level: 3, data: 59, direction: 'left', parent: 60},
-        {level: 3, data: 61, direction: 'right', parent: 60},
-        {level: 3, data: 75, direction: 'left', parent: 80},
-        {level: 3, data: 83, direction: 'right', parent: 80},
-        {level: 4, data: 73, direction: 'left', parent: 75},
-        {level: 5, data: 74, direction: 'right', parent: 73},
-      ]);
-    });
+    fn(root, 70);
+    expect(summarizeBinaryTree(root)).toEqual([
+      {level: 0, data: 50, direction: undefined, parent: undefined},
+      {level: 1, data: 30, direction: 'left', parent: 50},
+      {level: 1, data: 73, direction: 'right', parent: 50},
+      {level: 2, data: 20, direction: 'left', parent: 30},
+      {level: 2, data: 40, direction: 'right', parent: 30},
+      {level: 2, data: 60, direction: 'left', parent: 73},
+      {level: 2, data: 80, direction: 'right', parent: 73},
+      {level: 3, data: 59, direction: 'left', parent: 60},
+      {level: 3, data: 61, direction: 'right', parent: 60},
+      {level: 3, data: 75, direction: 'left', parent: 80},
+      {level: 3, data: 85, direction: 'right', parent: 80},
+      {level: 4, data: 74, direction: 'left', parent: 75},
+      {level: 4, data: 83, direction: 'left', parent: 85},
+      {level: 4, data: 86, direction: 'right', parent: 85},
+    ]);
   });
 
-  // TODO 또 내가 못찾은 케이스 있을 수 있으니 랜덤으로 삽입해서 랜덤으로 삭제해보자.
-  // TODO 처음부터 끝까지 다 삭제하자.
-  describe('root 노드 기준 왼쪽 노드 삭제하기', () => {
-    it('왼쪽 끝 리프노드 삭제해도 잘 삭제되야한다.', () => {
-      fn(root, 20);
-      expect(summarizeBinaryTree(root)).toEqual([
-        {level: 0, data: 50, direction: undefined, parent: undefined},
-        {level: 1, data: 30, direction: 'left', parent: 50},
-        {level: 1, data: 70, direction: 'right', parent: 50},
-        {level: 2, data: 40, direction: 'right', parent: 30},
-        {level: 2, data: 60, direction: 'left', parent: 70},
-        {level: 2, data: 80, direction: 'right', parent: 70},
-        {level: 3, data: 59, direction: 'left', parent: 60},
-        {level: 3, data: 61, direction: 'right', parent: 60},
-        {level: 3, data: 75, direction: 'left', parent: 80},
-        {level: 3, data: 85, direction: 'right', parent: 80},
-        {level: 4, data: 73, direction: 'left', parent: 75},
-        {level: 4, data: 83, direction: 'left', parent: 85},
-        {level: 4, data: 86, direction: 'right', parent: 85},
-        {level: 5, data: 74, direction: 'right', parent: 73},
-      ]);
-    });
-
-    it('왼쪽 끝에서 2 번쨰 리프노드를 삭제해도 잘 삭제되야한다.', () => {
-      fn(root, 40);
-      expect(summarizeBinaryTree(root)).toEqual([
-        {level: 0, data: 50, direction: undefined, parent: undefined},
-        {level: 1, data: 30, direction: 'left', parent: 50},
-        {level: 1, data: 70, direction: 'right', parent: 50},
-        {level: 2, data: 20, direction: 'left', parent: 30},
-        {level: 2, data: 60, direction: 'left', parent: 70},
-        {level: 2, data: 80, direction: 'right', parent: 70},
-        {level: 3, data: 59, direction: 'left', parent: 60},
-        {level: 3, data: 61, direction: 'right', parent: 60},
-        {level: 3, data: 75, direction: 'left', parent: 80},
-        {level: 3, data: 85, direction: 'right', parent: 80},
-        {level: 4, data: 73, direction: 'left', parent: 75},
-        {level: 4, data: 83, direction: 'left', parent: 85},
-        {level: 4, data: 86, direction: 'right', parent: 85},
-        {level: 5, data: 74, direction: 'right', parent: 73},
-      ]);
-    });
-
-    it('왼쪽에 자식이 있는 노드를 삭제할 경우 리프노드로 잘 대체되야한다.', () => {
-      fn(root, 30);
-      expect(summarizeBinaryTree(root)).toEqual([
-        {level: 0, data: 50, direction: undefined, parent: undefined},
-        {level: 1, data: 40, direction: 'left', parent: 50},
-        {level: 1, data: 70, direction: 'right', parent: 50},
-        {level: 2, data: 20, direction: 'left', parent: 40},
-        {level: 2, data: 60, direction: 'left', parent: 70},
-        {level: 2, data: 80, direction: 'right', parent: 70},
-        {level: 3, data: 59, direction: 'left', parent: 60},
-        {level: 3, data: 61, direction: 'right', parent: 60},
-        {level: 3, data: 75, direction: 'left', parent: 80},
-        {level: 3, data: 85, direction: 'right', parent: 80},
-        {level: 4, data: 73, direction: 'left', parent: 75},
-        {level: 4, data: 83, direction: 'left', parent: 85},
-        {level: 4, data: 86, direction: 'right', parent: 85},
-        {level: 5, data: 74, direction: 'right', parent: 73},
-      ]);
-    });
+  it('기타 > 루트 노드를 삭제해도 잘 삭제되야한다.', () => {
+    const root = new BinaryTreeNode(50);
+    root.left = new BinaryTreeNode(40);
+    root.right = new BinaryTreeNode(60);
+    expect(fn(root, 50)?.data).toBe(60);
+    expect(summarizeBinaryTree(root)).toEqual([
+      {level: 0, data: 60, direction: undefined, parent: undefined},
+      {level: 1, data: 40, direction: 'left', parent: 60},
+    ]);
   });
 });
+
+// TODO 또 내가 못찾은 케이스 있을 수 있으니 랜덤으로 삽입해서 랜덤으로 삭제해보자.
+// TODO 처음부터 끝까지 다 삭제하자.
+
+function createDeepBST() {
+  const root = new BinaryTreeNode(50);
+
+  // level 1
+  root.left = new BinaryTreeNode(30);
+  root.right = new BinaryTreeNode(70);
+
+  // level2
+  root.left.left = new BinaryTreeNode(20);
+  root.left.right = new BinaryTreeNode(40);
+  root.right.left = new BinaryTreeNode(60);
+  root.right.right = new BinaryTreeNode(80);
+
+  // level 3
+  root.right.left.left = new BinaryTreeNode(59);
+  root.right.left.right = new BinaryTreeNode(61);
+  root.right.right.left = new BinaryTreeNode(75);
+  root.right.right.right = new BinaryTreeNode(85);
+
+  // level4
+  root.right.right.left.left = new BinaryTreeNode(73);
+  root.right.right.right.left = new BinaryTreeNode(83);
+  root.right.right.right.right = new BinaryTreeNode(86);
+
+  // level5
+  root.right.right.left.left.right = new BinaryTreeNode(74);
+
+  return root;
+}
