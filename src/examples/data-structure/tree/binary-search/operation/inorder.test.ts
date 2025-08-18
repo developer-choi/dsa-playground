9f5f30b5a55c2ef945a87f655edd649d07d56033
Yu Jin Choe <forworkchoe@gmail.com>
2025-08-19 00:06:04 +0900

    기존 코드에서 Potential Successor를 구하는 부분을 잘못 이해해서 코드 수정

--- src/examples/data-structure/tree/binary-search/operation/inorder.test.ts
+++ src/examples/data-structure/tree/binary-search/operation/inorder.test.ts
@@ -1,8 +1,8 @@
 import {BinaryTreeNode} from '@/examples/data-structure/tree/complete-binary';
-import {inorderBST} from '@/examples/data-structure/tree/binary-search/operation/inorder';
+import {recursiveInorderSuccessorBST} from '@/examples/data-structure/tree/binary-search/operation/inorder';
 
 // yarn test src/examples/data-structure/tree/binary-search/operation/inorder.test.ts
-describe('inorderBST()', () => {
+describe('recursiveInorderBST()', () => {
   it('예제는 만족해야한다', () => {
     const root = new BinaryTreeNode(20);
     // level1
@@ -14,8 +14,8 @@ describe('inorderBST()', () => {
     // level3
     root.left.right.left = new BinaryTreeNode(10);
     root.left.right.right = new BinaryTreeNode(14);
-    expect(inorderBST(root, 8)).toBe(10);
-    expect(inorderBST(root, 10)).toBe(12);
-    expect(inorderBST(root, 14)).toBe(20);
+    expect(recursiveInorderSuccessorBST(root, 8)).toBe(10);
+    expect(recursiveInorderSuccessorBST(root, 10)).toBe(12);
+    expect(recursiveInorderSuccessorBST(root, 14)).toBe(20);
   });
 });
--- src/examples/data-structure/tree/binary-search/operation/inorder.ts
+++ src/examples/data-structure/tree/binary-search/operation/inorder.ts
@@ -1,20 +1,33 @@
 import {BinaryTreeNode} from '@/examples/data-structure/tree/complete-binary';
 import {determineBstDirection, getSuccessor} from '@/examples/data-structure/tree/binary-search';
 
-export function inorderBST(root: BinaryTreeNode<number> | undefined, target: number): number | undefined {
+/**
+ * URL: https://www.geeksforgeeks.org/dsa/inorder-successor-in-binary-search-tree/
+ * Doc: https://docs.google.com/document/d/1m-20HfhQLC125fren7qysz80xt9lyJdtKUhqGmLR9_g/edit?tab=t.0
+ * Time Complexity: O(h)
+ * Auxiliary Space: O(h)
+ */
+export function recursiveInorderSuccessorBST(root: BinaryTreeNode<number> | undefined, target: number): number | undefined {
   if (root === undefined) {
     return undefined;
   }
 
-  const history: number[] = [];
+  let successor: number | undefined = undefined;
 
   function recursive(node: BinaryTreeNode<number> | undefined) {
     if (!node) {
-      return;
+      return undefined;
+    }
+
+    /**
+     * If current node is greater , then it is a potential successor, we mark it as successor and proceed to left
+     * 공식문서에서 이 부분에 대한 코드
+     */
+    if (node.data > target) {
+      successor = node.data;
     }
 
     if (node.data !== target) {
-      history.unshift(node.data);
       const direction = determineBstDirection(node, target);
       return recursive(node[direction]);
     }
@@ -24,7 +37,7 @@ export function inorderBST(root: BinaryTreeNode<number> | undefined, target: num
       return getSuccessor(node).data;
       // Case 2. node.right가 없는 경우
     } else {
-      return history.find(value => value > node.data);
+      return successor;
     }
   }
 
