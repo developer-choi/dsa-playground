633ae6d2f3c36e139cf6f02f36d4a0bd8af53500
Yu Jin Choe <forworkchoe@gmail.com>
2025-08-24 23:55:24 +0900

    k 번째 작은 요소들의 총합 구현

--- src/examples/data-structure/tree/binary/search/application.ts
+++ src/examples/data-structure/tree/binary/search/application.ts
@@ -1,8 +1,10 @@
 import {BinaryTreeDirection, BinaryTreeNode} from '@/examples/data-structure/tree/binary';
+import {traverseAllNodes} from '@/examples/data-structure/tree/binary/traversal';
 
 /**
- * https://www.geeksforgeeks.org/dsa/find-the-minimum-element-in-a-binary-search-tree/#expected-approach-iterative-approach-on-time-and-o1-space
- * https://www.geeksforgeeks.org/dsa/find-the-node-with-maximum-value-in-a-binary-search-tree/#expected-approach-iterative-approach-on-time-and-o1-space
+ * URL: https://www.geeksforgeeks.org/dsa/find-the-minimum-element-in-a-binary-search-tree/#expected-approach-iterative-approach-on-time-and-o1-space
+ * URL: https://www.geeksforgeeks.org/dsa/find-the-node-with-maximum-value-in-a-binary-search-tree/#expected-approach-iterative-approach-on-time-and-o1-space
+ * Doc: https://docs.google.com/document/d/1jCfMEmybcdEaWi0f__mAV1kNolpttWlrBn2q-pjf9E4/edit?tab=t.0
  * 테스트 코드는 ./binary/application.test.ts에 통합되어있음.
  *
  * Time Complexity: O(h)
@@ -22,4 +24,36 @@ export function iterativeBoundaryBST(root: BinaryTreeNode<number> | undefined, b
   }
 
   return current.data;
+}
+
+/**
+ * URL: https://www.geeksforgeeks.org/dsa/sum-k-smallest-elements-bst/
+ * Doc: https://docs.google.com/document/d/1jCfMEmybcdEaWi0f__mAV1kNolpttWlrBn2q-pjf9E4/edit?tab=t.0
+ * Time Complexity: O(h + k) 최소 노드 찾아가는데 h, 이후 k개 순회
+ * Auxiliary Space: O(h)
+ */
+export function sumKSmallestBST(root: BinaryTreeNode<number> | undefined, k: number) {
+  if (root === undefined || k <= 0) {
+    return 0;
+  }
+
+  let count = 0;
+  let sum = 0;
+
+  /**
+   * 전체 노드를 순회하니까 O(n)인것같지만,
+   * 1. BST는 inorder로 순회하면 오름차순으로 순회를 하게 되고,
+   * 2. 다 세면 중간에 바로 break 해서 더 안세기 때문에
+   * O(n)이 아님.
+   */
+  for (const {node: {data}} of traverseAllNodes(root, 'inorder')) {
+    sum += data;
+    count++;
+
+    if (count >= k) {
+      break;
+    }
+  }
+
+  return sum;
 }
