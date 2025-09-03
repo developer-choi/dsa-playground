9c243c0d7338cff1aac450a19228dd624aaf8b53
Yu Jin Choe <forworkchoe@gmail.com>
2025-09-03 12:58:22 +0900

    LCA 구하기 2번째 풀이법에 대해 TODO 코드 추가

--- src/data-structure/tree/binary/lca.ts
+++ src/data-structure/tree/binary/lca.ts
@@ -57,3 +57,35 @@ export function findPath<D>(root: BinaryTreeNode<D> | undefined, target: D): D[]
   preorder(root);
   return paths;
 }
+
+/**
+ * URL: https://www.geeksforgeeks.org/dsa/lowest-common-ancestor-binary-tree-set-1/#expected-approach-on-time-and-oh-space
+ *
+ * 1. LCA 찾기
+ * 2. LCA 후보찾기
+ *
+ * TODO 전체적으로 다 절묘함. 뭔가 잘 이해가안됨.
+ */
+function unsolved<D>(root: BinaryTreeNode<D> | undefined, n1: D, n2: D): BinaryTreeNode<D> | undefined {
+  if (root === undefined)
+    return undefined;
+
+  // 역할 1. LCA 찾기
+  // TODO 여기서 바로 return 해도 되는가? 따져보면 맞긴한데 되게 절묘하네
+  // 케이스 1. root node, root.left node 2개가 인자로 들어오면 root로 잘 반환되긴 하니까.
+  // 케이스 2. 저 멀리 떨어진 2개의 노드가 들어온 경우 각 노드 하나씩 여기서 반환될거니까
+  if (root.data === n1 || root.data === n2)
+    return root;
+
+  let leftLca = unsolved(root.left, n1, n2);
+  let rightLca = unsolved(root.right, n1, n2);
+
+  // 역할 1. LCA 찾기
+  // 케이스 2. 저 멀리 떨어진 노드 2개가 들어온다고 하면 여기서 잘 LCA가 찾아길테니까
+  if (leftLca && rightLca)
+    return root;
+
+  // 역할 2. LCA 후보찾기
+  // 케이스 2. 저 멀리 떨어진 노드 2개가 들어온다고 하면 여기서 후보가 반환될테니까.
+  return leftLca ? leftLca : rightLca;
+}
