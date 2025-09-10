5fe0f3346af4984229b739b3b283243287e37909
Yu Jin Choe <forworkchoe@gmail.com>
2025-09-10 12:37:31 +0900

    max heap 체크하는 함수 GPT 코드리뷰 반영

--- src/data-structure/tree/binary/complete/heap/is-heap.test.ts
+++ src/data-structure/tree/binary/complete/heap/is-heap.test.ts
@@ -2,12 +2,15 @@ import {iterativeIsMaxHeap} from '@/data-structure/tree/binary/complete/heap/is-
 
 // yarn test src/data-structure/tree/binary/complete/heap/is-heap.test.ts
 describe('isMaxHeap()', () => {
-  it('예제는 만족해야한다.', () => {
+  it('should correctly identify valid and invalid max-heaps', () => {
     expect(iterativeIsMaxHeap([90, 15, 10, 7, 12, 2, 7, 3])).toBe(true);
+  });
+
+  it('should return false if the root node violates the heap property', () => {
     expect(iterativeIsMaxHeap([90, 1, 2, 7, 12, 2, 7, 3])).toBe(false);
   });
 
-  it('빈배열은 true가 반환되야한다.', () => {
+  it('should return true for an empty array', () => {
     expect(iterativeIsMaxHeap([])).toBe(true);
   });
 });
--- src/data-structure/tree/binary/complete/heap/is-heap.ts
+++ src/data-structure/tree/binary/complete/heap/is-heap.ts
@@ -1,7 +1,10 @@
 import {getFamilyIndexesFromCompleteBinaryTree} from '@/data-structure/tree/binary/complete/array';
 
 export function iterativeIsMaxHeap(array: number[]) {
-  for (let i = 0; i < array.length; i++) {
+  // 배열 전체를 순회 할 필요가 없고, 가장 마지막 부모 노드 까지만 순회하며 자식노드, 부모노드의 크기만 비교하면됨.
+  const {parent: lastParentIndex} = getFamilyIndexesFromCompleteBinaryTree(array, array.length - 1);
+
+  for (let i = 0; i <= lastParentIndex; i++) {
     const {left, right} = getFamilyIndexesFromCompleteBinaryTree(array, i);
     const data = array[i];
 
