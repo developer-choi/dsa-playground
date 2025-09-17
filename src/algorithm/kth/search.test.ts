4b0b608451525ff5de7e5ef7b3acdfce509c8d1b
Yu Jin Choe <forworkchoe@gmail.com>
2025-09-17 12:28:19 +0900

    기존 2번째 큰것만 찾는 테스트코드는 삭제하고 kth 찾는 테스트코드만 작성

--- src/algorithm/kth/search.test.ts
+++ src/algorithm/kth/search.test.ts
@@ -1,9 +1,35 @@
-import {findKthOrderValue} from '@/algorithm/kth/search';
+import {findKthOrderValue, OrderType} from '@/algorithm/kth/search';
 
 // yarn test src/algorithm/kth/search.test.ts
 describe('findKthOrderValue()', () => {
-  it('should return the k-th largest value for the given examples', () => {
-    expect(findKthOrderValue([12, 35, 1, 10, 34], 2, 'largest')).toBe(34);
-    expect(findKthOrderValue([-10, -5, -2], 2, 'largest')).toBe(-5);
+  describe('General cases', () => {
+    const testCases = [
+      // k-th Largest
+      {array: [12, 35, 1, 10, 34], k: 2, type: 'largest' as OrderType, expected: 34},
+      {array: [-10, -5, -2, -15], k: 2, type: 'largest' as OrderType, expected: -5},
+      {array: [10, 80, 30, 90, 40, 50, 70], k: 1, type: 'largest' as OrderType, expected: 90},
+      {array: [10, 80, 30, 90, 40, 50, 70], k: 7, type: 'largest' as OrderType, expected: 10},
+      // k-th Smallest
+      {array: [12, 35, 1, 10, 34], k: 3, type: 'smallest' as OrderType, expected: 12},
+      {array: [-10, -5, -2, -15], k: 2, type: 'smallest' as OrderType, expected: -10},
+      {array: [10, 80, 30, 90, 40, 50, 70], k: 1, type: 'smallest' as OrderType, expected: 10},
+      {array: [10, 80, 30, 90, 40, 50, 70], k: 7, type: 'smallest' as OrderType, expected: 90},
+    ];
+
+    it.each(testCases)('should return $expected for k=$k ($type) in array [$array]', ({array, k, type, expected}) => {
+      expect(findKthOrderValue(array, k, type)).toBe(expected);
+    });
+  });
+
+  describe('Edge cases', () => {
+    it('should return the sole element for a single-element array', () => {
+      expect(findKthOrderValue([100], 1, 'largest')).toBe(100);
+      expect(findKthOrderValue([100], 1, 'smallest')).toBe(100);
+    });
+
+    it('should return undefined for an empty array', () => {
+      expect(findKthOrderValue([], 1, 'largest')).toBe(undefined);
+      expect(findKthOrderValue([], 1, 'smallest')).toBe(undefined);
+    });
   });
 });
