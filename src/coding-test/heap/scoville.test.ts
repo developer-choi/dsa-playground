cf53b998b47b3e05f22de1716df45ea26514c2a3
Yu Jin Choe <forworkchoe@gmail.com>
2025-10-16 12:46:21 +0900

    스코빌 문제 설계 2회차

--- src/coding-test/heap/scoville.test.ts
+++ src/coding-test/heap/scoville.test.ts
@@ -4,31 +4,37 @@ import {heapScoville} from '@/coding-test/heap/scoville';
 describe('hwapScoville()', () => {
   describe('General cases', () => {
     it('예제는 만족해야한다', () => {
-      expect(heapScoville([1, 2, 3, 9, 10, 12], 2)).toBe(2);
+      expect(heapScoville([1, 2, 3, 9, 10, 12], 7)).toBe(2);
+    });
+  });
+
+  describe('Boundary cases', () => {
+    it('스코빌지수 배열이 0개인 경우에는 언제나 0이 반환되야한다.', () => {
+      expect(heapScoville([], 2)).toBe(0);
     });
   });
 
   describe('Edge cases', () => {
-    // TODO
+    it('스코빌지수 배열이 2개고, k보다 큰거 하나, 작은거 하나 있을 때도 잘 되야한다.', () => {
+      expect(heapScoville([3, 1], 2)).toBe(1);
+    });
+
     describe('아무리 섞어도 k 이상 될 수 없는 경우', () => {
-      // 배열길이가 2 이상
-      // 배열길이가 1
-      // 배열길이가 0
+      it('배열 길이가 2개인 경우에도 잘 되야한다.', () => {
+        expect(heapScoville([3, 4], 100)).toBe(-1);
+      });
+      it('배열 길이가 1개인 경우에도 잘 되야한다.', () => {
+        expect(heapScoville([3], 100)).toBe(-1);
+      });
     });
 
-    // TODO
     describe('안섞어도 전부 k 이상인 경우', () => {
-      // 배열길이가 2 이상
-      // 배열길이가 1
-      // 배열길이가 0
-    });
-
-    it('아무리 섞어도 k 이상이 될 수 없다면 -1을 반환해야한다.', () => {
-      expect(heapScoville([1, 2, 3], Number.MAX_SAFE_INTEGER)).toBe(-1);
-    });
-
-    it('섞지않아도 k를 만족할 수 있다면 0을 반환해야한다', () => {
-      expect(heapScoville([3, 4, 5], 1)).toBe(0);
+      it('배열 길이가 2개인 경우에도 잘 되야한다.', () => {
+        expect(heapScoville([3, 4], 0)).toBe(0);
+      });
+      it('배열 길이가 1개인 경우에도 잘 되야한다.', () => {
+        expect(heapScoville([3], 0)).toBe(0);
+      });
     });
   });
 });
