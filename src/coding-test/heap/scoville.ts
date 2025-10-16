f520ece3aa2174c5ce692aa598832a15298ed8e6
Yu Jin Choe <forworkchoe@gmail.com>
2025-10-16 12:55:22 +0900

    1회차 제출

--- src/coding-test/heap/scoville.ts
+++ src/coding-test/heap/scoville.ts
@@ -1,27 +1,55 @@
+import {MinHeap} from '@/data-structure/tree/binary/complete/heap';
+
 /**
  * URL: https://school.programmers.co.kr/learn/courses/30/lessons/42626
  * Doc: https://docs.google.com/document/d/1dUt9mYfzFzZBdQBK-qvHiyi2_6nEScqxEQd0IdvJs8c/edit?tab=t.0#heading=h.czr7uv2uqstm
  */
 export function heapScoville(scovilles: number[], k: number): number | -1 {
+  // k보다 커서 힙에서 가장 최근에 버려진 스코빌지수값
+  let latestScovileGreaterThanK: number | undefined = undefined;
+  const minHeap = new MinHeap();
+
+  for(const scoville of scovilles) {
+    if (scoville >= k) {
+      latestScovileGreaterThanK = scoville;
+    } else {
+      minHeap.add(scoville);
+    }
+  }
+
+  let count = 0;
+
+  while (minHeap.length >= 2) {
+    const minimum = minHeap.extractRoot() as number;
+    const second = minHeap.extractRoot() as number;
+    const sumScovile = minimum + second * 2;
+    count++; // 일단 섞었으니까 +1
+
+    if (sumScovile >= k) {
+      latestScovileGreaterThanK = sumScovile;
+    } else {
+      minHeap.add(sumScovile);
+    }
+  }
+
   /**
-   * // k보다 커서 힙에서 버려진 스코빌지수값
-   * const latestScovileGreaterThanK = undefined;
-   *
-   * scoviles를 순회하면서
-   * k 보다 큰건 일단 다 버리고 - latestScovileGreaterThanK 에 넣기
-   * min heap에 죄다 추가 한 다음 (제일 위에있는거 = 제일 작은거)
-   *
-   * 이제 이 heap 가지고 문제를 풀면됨.
-   *
-   * count = 0; 으로 초기화 하고,
-   *
-   * 아래 내용을 반복해야함. 언제까지? length가 1보다 클 때까지
-   *
-   * extractRoot()를 2번 한 다음 count +1 하고,
-   * k보다 작으면 다시 넣고, 크면 버리고.
-   *
-   * 그 다음, heap의 길이가 0이면? 섞었더니 전부다 k보다 커서 아무런문제가없는경우 count 리턴
-   * 그 외 (길이가 1이면) 그 값은 반드시 k보다 작은거니까 (합이 컸으면 애초에 버려져서 heap 사이즈 0됐음;) latestScovileGreaterThanK 값이 있으면 count+1 / 없으면 -1 (싹다 섞어봤지만 k보다 작아서)
+   * 모든 스코빌지수 혹은 섞은 스코빌지수들이 전부 k보다 커졌음
    */
-  return 1;
+  if (minHeap.length === 0) {
+    return count;
+  }
+
+  // else > 길이가 1이다 = 섞어도 스코빌지수가 k보다 작았다
+
+  // 마지막으로 남은 스코빌 지수가 k보다 작았지만, 직전에 남아있던 다른 스코빌지수와 섞어서 k보다 커질 수 있음.
+  if (latestScovileGreaterThanK !== undefined) {
+    return count + 1;
+  }
+
+  /**
+   * 죄다 섞어봤지만 k보다 작은 케이스임.
+   * latestScovileGreaterThanK 도 없었고,
+   * 마지막으로 힙에 남은 스코빌 지수 조차도 k보다 작으니까.
+   */
+  return -1;
 }
