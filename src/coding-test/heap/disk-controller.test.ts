ea77d30a1d1642df6e2fc33793a5df12f4209ec1
Yu Jin Choe <forworkchoe@gmail.com>
2025-10-17 12:46:14 +0900

    디스크컨트롤러 설계추가

---
+++ src/coding-test/heap/disk-controller.test.ts
@@ -0,0 +1,31 @@
+import {diskController} from '@/coding-test/heap/disk-controller';
+
+// yarn test src/coding-test/heap/disk-controller.test.ts
+describe('diskController()', () => {
+  describe('General cases', () => {
+    it('예제는 만족해야한다', () => {
+      // (3-0) + (8-3) + (17-1) = 3 + 5 + 16 = 24. 24/3 = 8
+      expect(diskController([[0, 3], [1, 9], [3, 5]])).toBe(8);
+    });
+  });
+
+  describe('Boundary cases', () => {
+    it('빈배열이 전달되면 0이 반환되야한다.', () => {
+      expect(diskController([])).toBe(0);
+    });
+
+    it('작업이 하나만 있을 경우, 해당 작업의 소요 시간만 반환해야 한다.', () => {
+      expect(diskController([[0, 10]])).toBe(10);
+    });
+  });
+
+  describe('Edge cases', () => {
+    it('아주나중에 시작되는것도 잘 되야한다', () => {
+      expect(diskController([[100, 3]])).toBe(3);
+    });
+
+    it('동시에 시작하는 작업이 있더라도 잘 동작해야한다.', () => {
+      expect(diskController([[0, 3], [0, 2], [0, 1]])).toBe(3);
+    });
+  });
+});
---
+++ src/coding-test/heap/disk-controller.ts
@@ -0,0 +1,23 @@
+/**
+ * URL: https://school.programmers.co.kr/learn/courses/30/lessons/42627
+ * Doc: https://docs.google.com/document/d/1dUt9mYfzFzZBdQBK-qvHiyi2_6nEScqxEQd0IdvJs8c/edit?tab=t.0#heading=h.czr7uv2uqstm
+ */
+export function diskController(jobs: [number, number][]): number {
+  /**
+   * accumaltedTime = 0 선언해서 계속 더해서 나중에 반환하는 컨셉으로.
+   * const remainJobs = new MinHeap();
+   * const processingJob = undefined
+   * while remainJobs가 0 이상이면 계속 돌아야하고
+   */
+
+  /**
+   * jobs 순회돌려서 heap에 넣기
+   */
+
+  /**
+   * processingJob이 없으면 remainJob의 root 시작시간이 acc값 이하인지 체크해서 작으면 넣기
+   * processingJob 소요시간이 1 이상이면 --하고 & acc++;
+   * 아니면 undefined 할당하기
+   */
+  return 0;
+}
