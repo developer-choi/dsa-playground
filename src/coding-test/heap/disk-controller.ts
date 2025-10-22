29c7bdeded984f50f81cef8207282aeab83e6d38
Yu Jin Choe <forworkchoe@gmail.com>
2025-10-23 06:40:31 +0900

    1회차 제출

--- src/coding-test/heap/disk-controller.ts
+++ src/coding-test/heap/disk-controller.ts
@@ -1,23 +1,49 @@
+import {MinHeap} from '@/data-structure/tree/binary/complete/heap';
+
+export type DiskControllerJob = [number, number];
+
 /**
  * URL: https://school.programmers.co.kr/learn/courses/30/lessons/42627
  * Doc: https://docs.google.com/document/d/1dUt9mYfzFzZBdQBK-qvHiyi2_6nEScqxEQd0IdvJs8c/edit?tab=t.0#heading=h.czr7uv2uqstm
  */
-export function diskController(jobs: [number, number][]): number {
-  /**
-   * accumaltedTime = 0 선언해서 계속 더해서 나중에 반환하는 컨셉으로.
-   * const remainJobs = new MinHeap();
-   * const processingJob = undefined
-   * while remainJobs가 0 이상이면 계속 돌아야하고
-   */
+export function diskController(jobs: DiskControllerJob[]): number {
+  if (jobs.length === 0) {
+    return 0;
+  }
 
-  /**
-   * jobs 순회돌려서 heap에 넣기
-   */
+  let accumulatedTime = 0;
+  let accumulatedDurationTime = 0;
 
-  /**
-   * processingJob이 없으면 remainJob의 root 시작시간이 acc값 이하인지 체크해서 작으면 넣기
-   * processingJob 소요시간이 1 이상이면 --하고 & acc++;
-   * 아니면 undefined 할당하기
-   */
-  return 0;
+  const remainJobs = new MinHeap<DiskControllerJob>((a, b) => {
+    if (a[1] !== b[1]) {
+      return a[1] - b[1];
+    }
+
+    return a[0] - b[0];
+  });
+
+  for (const job of jobs) {
+    remainJobs.add(job);
+  }
+
+  let processingJob: DiskControllerJob | undefined = undefined;
+
+  while (remainJobs.length > 0 || processingJob !== undefined) {
+    if (processingJob === undefined && (remainJobs.peek() as DiskControllerJob)[0] <= accumulatedTime) {
+      processingJob = remainJobs.extractRoot() as DiskControllerJob;
+    }
+
+    if (processingJob && processingJob[1] >= 1) {
+      processingJob[1]--;
+    }
+
+    accumulatedTime++;
+
+    if (processingJob && processingJob[1] === 0) {
+      accumulatedDurationTime += (accumulatedTime - processingJob[0]);
+      processingJob = undefined;
+    }
+  }
+
+  return Math.floor(accumulatedDurationTime / jobs.length);
 }
