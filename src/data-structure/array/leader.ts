fda4c86341b4038db6224cce9a0d32d712ff28c8
Yu Jin Choe <forworkchoe@gmail.com>
2025-10-24 12:53:27 +0900

    Leaders in array 문제 Brute Force 풀이 추가

--- src/data-structure/array/leader.ts
+++ src/data-structure/array/leader.ts
@@ -6,16 +6,20 @@
 export function leadersInArrayUsingBruteForce(array: number[]): number[] {
   const result: number[] = [];
 
-  /**
-   * for 1. i는 0부터 array 길이까지 시작
-   * let isLeader = true로 초기화
-   *
-   * for 2. j는 i+1 부터 array 길이까지 시작
-   * j가 i부터 시작하지않는 이유는 그럴 필요가 없어서. 어차피 i와 같을텐데 뭐하러.
-   * i번쨰 요소가 j번째 요소보다 작거나 같으면 리더가 아니라는 소리이기 때문에 isLeader = false로 하고 즉시 배열중단
-   *
-   * 여전히 isLeader가 true면 우측으로 순회하는 내내 leader보다 큰게 없었다는 뜻이 되기 때문에
-   * result에 추가
-   */
+  for (let i = 0; i < array.length; i++) {
+    let isLeader = true;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if(array[i] <= array[j]) {
+        isLeader = false;
+        break;
+      }
+    }
+
+    if (isLeader) {
+      result.push(array[i]);
+    }
+  }
+
   return result;
 }
