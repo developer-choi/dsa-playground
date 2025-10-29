dcfe033cd63f5f9b608aa444449bd88f6dcd0a8b
Yu Jin Choe <forworkchoe@gmail.com>
2025-10-29 12:57:33 +0900

    배열 회전시키기 One by one 풀이 제출

--- src/data-structure/array/rotate.ts
+++ src/data-structure/array/rotate.ts
@@ -1,19 +1,22 @@
 /**
  * URL: https://www.geeksforgeeks.org/dsa/complete-guide-on-array-rotations/#1-rotate-one-by-one
  * Doc: https://docs.google.com/document/d/1x11Iyb-uSmG4Jr30_8Cn2IMsXneM7y11zCaq0VmW4kA/edit?tab=t.0
+ * Time Complexity: O(n * d)
  */
 export function rotateArrayUsingOneByOne(array: number[], distance: number, direction: 'left' | 'right'): number[] {
-  /**
-   * distance 만큼 반복하면서,
-   * target = direction === 'left' ? array[0] : array[마지막]
-   *
-   * if(오른쪽이면)
-   * 맨 왼쪽에 맨 마지막꺼 추가하고
-   * 맨 오른쪽거를 삭제
-   *
-   * else면
-   * 맨 오른쪽에 맨 왼쪽거 추가하고
-   * 맨 왼쪽꺼를 삭제
-   */
-  return [];
+  const result = [...array];
+  const optimizatedRotate = distance % array.length;
+
+  // O(d)
+  for (let i = 0; i < optimizatedRotate; i++) {
+    const lastIndex = result.length - 1;
+    const target = direction === 'left' ? result[0] : result[lastIndex];
+    const toCreateIndex = direction === 'left' ? lastIndex : 0;
+    const toRemoveIndex = direction === 'left' ? 0 : lastIndex;
+
+    // O(n)
+    result.splice(toRemoveIndex, 1);
+    result.splice(toCreateIndex, 0, target);
+  }
+  return result;
 }
