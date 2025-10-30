c7c4d028f2e8cd3d93ed756229c5857a36f1728a
Yu Jin Choe <forworkchoe@gmail.com>
2025-10-30 12:31:26 +0900

    최소로 더해서 모든요소 똑같이 만들기 문제 풀이 2회차 풀이 제출

--- src/data-structure/array/minium-increase-by-k.ts
+++ src/data-structure/array/minium-increase-by-k.ts
@@ -1,12 +1,10 @@
-import {calculateNumberTotal} from '@forworkchoe/core/utils';
-
 /**
  * URL: https://www.geeksforgeeks.org/dsa/minimum-increment-k-operations-make-elements-equal/
  * Doc: https://docs.google.com/document/d/1x11Iyb-uSmG4Jr30_8Cn2IMsXneM7y11zCaq0VmW4kA/edit?tab=t.0
  */
 export function miniumIncreaseByK(array: number[], k: number): number | -1 {
   const max = Math.max(...array);
-  const diffCounts: number[] = [];
+  let sum = 0;
 
   for (const data of array) {
     if (data === max) {
@@ -19,8 +17,8 @@ export function miniumIncreaseByK(array: number[], k: number): number | -1 {
       return -1;
     }
 
-    diffCounts.push(diff / k);
+    sum += diff / k;
   }
 
-  return calculateNumberTotal(diffCounts);
+  return sum;
 }
