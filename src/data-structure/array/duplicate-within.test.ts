316eb82a251eb3e2c532cb98f512aa8dbf614132
Yu Jin Choe <forworkchoe@gmail.com>
2025-10-30 22:19:55 +0900

    k 거리 내에 중복된 요소가 하나라도 있는지 체크하는 함수 Brute Force 알고리즘 1회차 제출

--- src/data-structure/array/duplicate-within.test.ts
+++ src/data-structure/array/duplicate-within.test.ts
@@ -4,7 +4,7 @@ const algorithms = [
   {name: 'Brute Force', fn: duplicateWithinUsingBruteForce},
 ];
 
-// yarn test src/algorithm/sort/sort.test.ts
+// yarn test src/data-structure/array/duplicate-within.test.ts
 describe.each(algorithms)('Duplicate within k distance algorithm > $name', ({fn}) => {
   describe('General cases', () => {
     it('거리가 k와 같은 중복 요소가 있는 경우 true를 반환해야 한다', () => {
--- src/data-structure/array/duplicate-within.ts
+++ src/data-structure/array/duplicate-within.ts
@@ -1,17 +1,16 @@
 /**
  * URL: https://www.geeksforgeeks.org/dsa/check-given-array-contains-duplicate-elements-within-k-distance/
  * Doc: https://docs.google.com/document/d/1JvljjsXl5iYjEGn9QCp-hKgioaxNZSI9S4-MHLpci7c/edit?tab=t.0
+ * Time Complexity: O(n * d)
  */
 export function duplicateWithinUsingBruteForce(array: number[], distance: number): boolean {
-  /**
-   * array를 처음부터 끝까지 순회하면서 for i = 0;
-   * let hasDuplicatedValue = false
-   *
-   * for j = i ; j < i + distance 까지 순회하면서
-   * j 인덱스가 i인덱스 요소와 하나라도 같은지 체크해서 같은게 있으면 hasDuplicatedValue = true로 하고
-   *
-   * hasDuplicatedValue 가 true이면 넘어가고
-   * false이면 즉시 return false
-   */
+  for (let i = 0; i < array.length - distance; i++) {
+    for (let j = i + 1; j <= i + distance; j++) {
+      if (array[j] === array[i]) {
+        return true;
+      }
+    }
+  }
+
   return false;
 }
