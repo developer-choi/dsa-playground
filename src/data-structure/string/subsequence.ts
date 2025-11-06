0d955a99d1d2685a66dfce4dc2430458bb419cf3
Yu Jin Choe <forworkchoe@gmail.com>
2025-11-06 12:44:28 +0900

    순회방식으로 subsequence 문제 풀이 제출

--- src/data-structure/string/subsequence.ts
+++ src/data-structure/string/subsequence.ts
@@ -1,16 +1,17 @@
 /**
  * URL: https://www.geeksforgeeks.org/dsa/given-two-strings-find-first-string-subsequence-second/
  * Doc: https://docs.google.com/document/d/1I5vzUlHqNCIZCx1rc3N6QAD1DRRKrCyOIm1cz6BYvFA/edit?tab=t.0
+ * Time Complexity: O(n) - original의 길이만큼
  */
 export function subsequenceUsingIterative(sequence: string, original: string): boolean {
-  /**
-   * sequence을 배열로 쪼게서 저장하고
-   * sequenceIndex를 0으로 선언하고
-   *
-   * original를 처음부터 끝까지 순회하면서
-   * 그 글자가 sequence의 sequencendex 요소와 같으면 sequenceIndex를 + 1
-   *
-   * sequenceIndex의 값이 sequence 길이와 같으면 바로 return true 해서 빨리종료하기
-   */
-  return false;
+  const sequenceArray = sequence.split('');
+  let sequenceIndex = 0;
+
+  for(let i = 0; i < original.length && sequenceIndex < sequence.length; i++) {
+    if(original[i] === sequenceArray[sequenceIndex]) {
+      sequenceIndex++;
+    }
+  }
+
+  return sequenceIndex >= sequence.length;
 }
