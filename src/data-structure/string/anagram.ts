70e85051a5570e86cf631dd23d83872f08e1165e
Yu Jin Choe <forworkchoe@gmail.com>
2025-11-07 06:50:26 +0900

    anagram 1회차 구현

--- src/data-structure/string/anagram.ts
+++ src/data-structure/string/anagram.ts
@@ -1,12 +1,24 @@
+/**
+ * URL: https://www.geeksforgeeks.org/dsa/check-whether-two-strings-are-anagram-of-each-other/#expected-approach-1-using-hash-map-or-dictionary
+ * Doc: https://docs.google.com/document/d/1I5vzUlHqNCIZCx1rc3N6QAD1DRRKrCyOIm1cz6BYvFA/edit?tab=t.0
+ * Time Complexity: O(n) - 순회가 text 2개의 길이 + 유일한 문자갯수 만큼만 발생함, 그래서 결국 O(n)임
+ */
 export function anagramUsingHashmap(text1: string, text2: string): boolean {
-  /**
-   * map 선언
-   *
-   * text1 돌면서 map에 글자 1개를 키값으로 count값을 넣고
-   *
-   * text2를 돌면서 map에 글자 1개를 키값으로 count만큼 -1씩 하면됨
-   */
+  const map = new Map<string, number>();
 
-  // map 순회하면서 count가 전부 0인지를 반환하면됨.
+  for (const char of text1) {
+    map.set(char, (map.get(char) ?? 0) + 1);
+  }
+
+  for (const char of text2) {
+    map.set(char, (map.get(char) ?? 0) - 1);
+  }
+
+  for (const [, value] of map) {
+    if (value !== 0) {
+      return false;
+    }
+  }
+
   return true;
 }
