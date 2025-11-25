c11aa845e3e3511089dabc01520ccf3a163e3fe7
Yu Jin Choe <forworkchoe@gmail.com>
2025-11-25 12:59:18 +0900

    서버증설문제 풀던중

--- src/coding-test/array/level2/server-expansion.test.ts
+++ src/coding-test/array/level2/server-expansion.test.ts
@@ -1,6 +1,19 @@
+import {serverExpansionSolution} from './server-expansion';
+
+// yarn test src/coding-test/array/level2/server-expansion.test.ts
 describe('serverExpansionSolution()', () => {
   describe('General cases', () => {
+    it('should return 7 when players=[0, 2, 3, 3, 1, 2, 0, 0, 0, 0, 4, 2, 0, 6, 0, 4, 2, 13, 3, 5, 10, 0, 1, 5], m=3, k=5', () => {
+      expect(serverExpansionSolution([0, 2, 3, 3, 1, 2, 0, 0, 0, 0, 4, 2, 0, 6, 0, 4, 2, 13, 3, 5, 10, 0, 1, 5], 3, 5)).toBe(7);
+    });
 
+    it('should return 11 when players=[0, 0, 0, 10, 0, 12, 0, 15, 0, 1, 0, 1, 0, 0, 0, 5, 0, 0, 11, 0, 8, 0, 0, 0], m=5, k=1', () => {
+      expect(serverExpansionSolution([0, 0, 0, 10, 0, 12, 0, 15, 0, 1, 0, 1, 0, 0, 0, 5, 0, 0, 11, 0, 8, 0, 0, 0], 5, 1)).toBe(11);
+    });
+
+    it('should return 12 when players=[0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 5, 0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1], m=1, k=1', () => {
+      expect(serverExpansionSolution([0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 5, 0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1], 1, 1)).toBe(12);
+    });
   });
 
   describe('Boundary cases', () => {
--- src/coding-test/array/level2/server-expansion.ts
+++ src/coding-test/array/level2/server-expansion.ts
@@ -2,26 +2,23 @@
  * URL: https://school.programmers.co.kr/learn/courses/30/lessons/389479
  * Doc: https://docs.google.com/document/d/1x11Iyb-uSmG4Jr30_8Cn2IMsXneM7y11zCaq0VmW4kA/edit?tab=t.0
  */
-export function serverExpansionSolution(players: number[], maxUserLimit: number, operatingTime: number) {
-  /**
-   * let 현재 운영중인 서버 = []; {startedHours: number}[]가 좋겠음.
-   * let 누적 서버 증설횟수 = 0;
-   *
-   * players 순회 돌면서 - O(n)
-   * const isExceed = boolean 으로 계산 하고, boolean에는 현재 서버갯수와 maxUserLimit으로 계산.
-   *
-   * if(isExceed) 이면
-   * 서버댓수 + n
-   * 누적서버증설횟수 + n
-   *
-   * 운영중인 서버들 중 operatingTime을 지난 서버들은 모두 종료처리
-   */
+export function serverExpansionSolution(players: number[], maxUserLimitPerServer: number, operatingTimeLimit: number) {
+  let operatingServers: {startedHours: number; count: number;}[] = [];
+  let accumalateServerIncreasementCount = 0;
 
-  /** TODO
-   * 음 시간복잡도가
-   * 1. 순회 - O(n)
-   * 2. 종료되야하는 서버 찾기 - 음... 뭐라고 표시하지 음...
-   * 암튼 이거 2개 곱해야함.
-   */
-  return 0;
+  for (let i = 0 ; i < players.length; i++) {
+    const needServerCapacity = Math.floor(players[i] / maxUserLimitPerServer) * operatingTimeLimit;
+    const currentCapacity = (operatingServers.length + 1) * maxUserLimitPerServer;
+
+    const isExceed = needServerCapacity > currentCapacity;
+
+    if(isExceed) {
+      operatingServers.push({startedHours: i, count: needServerCapacity - operatingServers.length});
+      accumalateServerIncreasementCount++;
+    }
+
+    operatingServers = operatingServers.filter(({startedHours}) => (i - startedHours + 1) < operatingTimeLimit);
+  }
+
+  return accumalateServerIncreasementCount;
 }
