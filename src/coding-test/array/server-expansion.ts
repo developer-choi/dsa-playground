f1e2971d6783699c45380f55c9280c0887e5c6cd
Yu Jin Choe <forworkchoe@gmail.com>
2025-12-17 12:46:35 +0900

    서버 증설횟수 구하기 문제 구현 1회차

--- src/coding-test/array/server-expansion.ts
+++ src/coding-test/array/server-expansion.ts
@@ -2,20 +2,23 @@
  * URL: https://school.programmers.co.kr/learn/courses/30/lessons/389479
  * Doc: https://docs.google.com/document/d/1x11Iyb-uSmG4Jr30_8Cn2IMsXneM7y11zCaq0VmW4kA/edit?tab=t.0
  */
-export function getAccumulatedServerIncreasementCount(playerCounts: number[], maxPlayerCount: number, serverOperatingTime: number): number {
-  /**
-   * let 누적 서버 증가 횟수 저장
-   * let 현재 운영중인 서버 목록 (추가된 시간, 댓수)
-   *
-   * 0부터 24시까지 players를 순회하면서
-   * 1시간 마다 뭔가를 세는 컨셉으로 진행.
-   *
-   * const 현재 운영중인 서버 댓수 계산
-   * const 현재 감당할 수 있는 최대 사용자 명수 계산
-   * 현재 감당할 수 있는 최대 명수와 playerCount를 비교해서 추가로 필요한 서버 댓수를 뽑아온 다음
-   * 위 let 변수 2개에 반영
-   *
-   * 운영중인 서버 순회하면서 serverOperatingTime을 지난 서버는 운영서버 목록에서 삭제
-   */
-  return 0;
+export function getAccumulatedServerIncreasementCount(playerCounts: number[], playerCapacityOfServer: number, serverOperatingTime: number): number {
+  let accumulatedServerIncreasementCount = 0;
+  let operatingServers: {count: number, increasedTime: number}[] = [];
+
+  for(let i = 0; i < playerCounts.length; i++) {
+    const playerCount = playerCounts[i];
+    const operatingServerCount = operatingServers.reduce((a, b) => a + b.count, 0);
+    const maxPlayerCapacity = (operatingServerCount + 1) * playerCapacityOfServer - 1;
+    const needServerCount = maxPlayerCapacity > playerCount ? 0 : Math.ceil((playerCount - maxPlayerCapacity) / playerCapacityOfServer);
+    accumulatedServerIncreasementCount += needServerCount;
+
+    if (needServerCount) {
+      operatingServers.push({count: needServerCount, increasedTime: i});
+    }
+
+    operatingServers = operatingServers.filter((({increasedTime}) => (i - increasedTime) < serverOperatingTime - 1));
+  }
+
+  return accumulatedServerIncreasementCount;
 }
