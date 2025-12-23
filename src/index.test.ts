4b0c985e7c3154355d05b2035a2c74f4689c52c8
Yu Jin Choe <forworkchoe@gmail.com>
2025-12-23 20:05:59 +0900

    알고보니 2nd parameter가 최대 길이였어서 알고리즘이 잘못됨

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,7 +4,11 @@ import {getMaxProfit} from '@/index';
 describe('getMaxProfit()', () => {
   describe('General cases', () => {
     it('예제는 만족해야한다', () => {
-      expect(getMaxProfit([-3, 4, 3, -2, 2, 5], 4)).toBe(4);
+      expect(getMaxProfit([-3, 4, 3, -2, 2, 5], 4)).toBe(8);
+    });
+
+    it('틀린 예제도 만족해야한다.', () => {
+      expect(getMaxProfit([2, 5, -7, 8, -6, 4, 1, -9], 5)).toBe(8);
     });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
-export function getMaxProfit(profitAndLoss: number[], subArrayLength: number): number {
-  const max = 0;
+export function getMaxProfit(profitAndLoss: number[], subArrayMaxLength: number): number {
+  let max = 0;
+  const window: number[] = [];
+  let sumOfWindow = 0;
 
-  /**
-   * let window: number[] = [];
-   * let sumOfWindow = 0;
-   *
-   * 배열 첨부터 끝까지 순회하면서 O(n)
-   * 윈도우 사이즈보다 작으면 총합 구하지말고 계속 집어넣고
-   * 윈도우 사이즈보다 크면 첫 요소 빼내와서 sum에서 제외하고 새 요소 집어넣고 sum에 더하기
-   * 현재 sum값은 계속 max값에 집어넣고
-   */
+  for(const item of profitAndLoss) {
+    window.push(item);
+    sumOfWindow += item;
+
+    if (window.length < subArrayMaxLength) {
+      continue;
+    }
+
+    max = Math.max(max, sumOfWindow);
+
+    const firstItem = window.shift() as number;
+    sumOfWindow -= firstItem;
+  }
+
   return max;
 }
