ae2c0b5128ed505de522021fa98b6009096db637
Yu Jin Choe <forworkchoe@gmail.com>
2025-12-23 20:11:22 +0900

    1번문제 잘 모르겠음...

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,11 @@
 export function getMaxProfit(profitAndLoss: number[], subArrayMaxLength: number): number {
   let max = 0;
-  const window: number[] = [];
-  let sumOfWindow = 0;
 
-  for(const item of profitAndLoss) {
-    window.push(item);
-    sumOfWindow += item;
-
-    if (window.length < subArrayMaxLength) {
-      continue;
-    }
-
-    max = Math.max(max, sumOfWindow);
-
-    const firstItem = window.shift() as number;
-    sumOfWindow -= firstItem;
-  }
+  /**
+   * 현재 배열안에 가장 최대값을 알아낼 수 있으면 그냥 계속 슬라이딩 윈도우 돌릴텐데.
+   * [a, b, c, d]
+   * a, b, c, d, ab, bc, cd, abc, bcd, abcd 경우의 수 너무많음...
+   */
 
   return max;
 }
