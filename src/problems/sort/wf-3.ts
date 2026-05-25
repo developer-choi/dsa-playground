de8f655692b7f26359f74779d1704b5e07c22f60
Yu Jin Choe <forworkchoe@gmail.com>
2026-05-25 18:48:57 +0900

    feat(sort): 다시 풀이 삭제

--- src/problems/sort/wf-3.ts
+++ src/problems/sort/wf-3.ts
@@ -1,25 +1,3 @@
 export function sort(grades: number[]): number[] {
-  const scoreRecord: Record<number, {count: number, upper: number;}> = {};
-
-  for (const grade of grades) {
-    if (grade in scoreRecord) {
-      scoreRecord[grade].count++;
-
-    } else {
-      scoreRecord[grade] = {
-        count: 1,
-        upper: 0
-      };
-    }
-  }
-
-  const sorted = Object.entries(scoreRecord).sort((a, b) => Number(a[0]) - Number(b[0]));
-  let acc = 0;
-
-  for(const [, value] of sorted) {
-    value.upper = grades.length - acc - value.count;
-    acc += value.count;
-  }
-
-  return grades.map(grade => scoreRecord[grade].upper + 1);
+  return [];
 }
