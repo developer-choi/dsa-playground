187bf24d9324a8e822f56675ffaee06e4530ff9b
Yu Jin Choe <forworkchoe@gmail.com>
2026-05-25 18:59:18 +0900

    feat(sort): wf-3 접근법 작성

--- src/problems/sort/wf-3.test.ts
+++ src/problems/sort/wf-3.test.ts
@@ -1,22 +1,32 @@
-import { sort } from './wf-3';
-import { compareFunctionsWithRandomInputs } from '@/utils/jest';
+import {sort} from './wf-3';
+import {compareFunctionsWithRandomInputs} from '@/utils/jest';
+
 
 const solutions = [
-  { name: 'sort', fn: sort },
+  {name: 'sort', fn: sort},
 ];
 
-describe.each(solutions)('등수 매기기 > $name', ({ fn }) => {
+describe.each(solutions)('등수 매기기 > $name', ({fn}) => {
   describe('General cases', () => {
     it('각 학생의 등수를 반환하며 동점자는 같은 등수를 가진다', () => {
-      expect(fn([2, 2, 1])).toEqual([1, 1, 3]);
-      expect(fn([3, 2, 1, 2])).toEqual([1, 2, 4, 2]);
+      expect(fn([4, 3, 2, 1])).toEqual([1, 2, 3, 4]);
+    });
+
+    it('같은 점수가 있으면 등수도 똑같이 나와야한다.', () => {
+      expect(fn([4, 4, 3])).toEqual([1, 1, 3]);
     });
   });
 
   describe('Boundary cases', () => {
+    it('모두 같은 점수면 모두 1등으로 나와야한다.', () => {
+      expect(fn([1, 1, 1])).toEqual([1, 1, 1]);
+    });
   });
 
   describe('Edge cases', () => {
+    it('빈배열이면 빈배열이 반환되야한다.', () => {
+      expect(fn([])).toEqual([]);
+    });
   });
 
   describe('Random', () => {
@@ -27,7 +37,7 @@ describe.each(solutions)('등수 매기기 > $name', ({ fn }) => {
           grades.map(g => grades.filter(other => other > g).length + 1),
         generateInput: () => {
           const n = Math.floor(Math.random() * 40) + 10;
-          const grades = Array.from({ length: n }, () => Math.floor(Math.random() * 10) + 1);
+          const grades = Array.from({length: n}, () => Math.floor(Math.random() * 10) + 1);
           return [[grades]] as const;
         },
         iterationCount: 1000,
