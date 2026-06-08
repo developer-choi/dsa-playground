e028e693db5a02f011e43fefee087e64ea6c89f5
Yu Jin Choe <forworkchoe@gmail.com>
2026-06-08 19:45:54 +0900

    feat(problem): wf-1 풀이 작성

--- src/problems/implementation/wf-1.ts
+++ src/problems/implementation/wf-1.ts
@@ -4,20 +4,25 @@
  */
 
 export function simulation(day: number, k: number): number[] {
-  /**
-   * isWeekend() 함수를 사전에 하나 만들고,
-   *
-   * 1월부터 12월이 고정이기 때문에, 12개월 값을 저장할 배열 하나 만들고,
-   * map 순회하면서 각 월의 k일이 주말인지 isWeekend()를 통해 알아내서
-   * true이면 1 / 아니면 0을 반환한다.
-   */
+  return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(month => {
+    const resultDay = isWeekend(day, {month, date: k});
 
-  /**
-   * isWeekend() 전달된 날짜가 주말인지 아닌지 판단하는 함수.
-   * ### Parameter
-   * - year를 모르기때문에, 기준이 될 1월 1일의 dayOfNewYear값
-   * - 주말인지 아닌지를 알고싶은 날짜값. 형식은, targetDate (month, date)
-   */
+    if (resultDay === 5 || resultDay === 6) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+}
 
-  return [];
+function isWeekend(dayOfNewYear: number, target: {month: number, date: number}) {
+  return (target.date - 1 + dayOfNewYear + ACCUMULATED_DATE[target.month - 1]) % 7;
+}
+
+const ORIGINAL_DATE = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+const ACCUMULATED_DATE: number[] = [0];
+
+for (let i = 1; i < ORIGINAL_DATE.length; i++) {
+  ACCUMULATED_DATE[i] = ACCUMULATED_DATE[i - 1] + ORIGINAL_DATE[i - 1];
 }
