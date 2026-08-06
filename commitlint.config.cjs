9958dd54ab4e3e4d4b25a264b4ebbed5c97e39ef
Yu Jin Choe <forworkchoe@gmail.com>
2026-08-07 08:33:33 +0900

    refactor(config): commitlint에 subject-korean 한글 필수 규칙을 추가한다

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,5 +1,15 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  plugins: [
+    {
+      rules: {
+        'subject-korean': ({ subject }) => {
+          const hasKorean = /[\uAC00-\uD7AF]/.test(subject);
+          return [hasKorean, '커밋 메시지(subject)에 한글이 포함되어야 합니다 (한글 필수)'];
+        },
+      },
+    },
+  ],
   rules: {
     'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'test']],
     'scope-enum': [
@@ -16,5 +26,6 @@ module.exports = {
     'scope-empty': [2, 'never'],
     // 한국어 커밋엔 대소문자 개념이 없어 무의미하고, README·TS 같은 정당한 약어/파일명만 막는다. 비활성화.
     'subject-case': [0],
+    'subject-korean': [2, 'always'],
   },
 };
